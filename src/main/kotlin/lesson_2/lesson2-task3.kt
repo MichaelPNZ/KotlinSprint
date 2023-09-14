be6cdb0f03d5e17635b961b8c6d198b7934e6843
PozyalovMihail <pozyalov@gmail.com>
2023-09-14 21:48:22 +0300

    KS-2-3 Task3 completed.

--- src/main/kotlin/lesson_2/lesson2-task3.kt
+++ src/main/kotlin/lesson_2/lesson2-task3.kt
@@ -1,2 +1,16 @@
 package lesson_2
 
+fun main() {
+    val hour = 9
+    val minute = 39
+    val travelTime = 457
+
+    val travelTimeOfHour = travelTime / 60
+    val travelTimeOfMinute = travelTime % 60
+
+    val arrivalTimeMinute = minute + travelTimeOfMinute
+    val arrivalHour = hour + travelTimeOfHour + (arrivalTimeMinute / 60)
+    val arrivalMinute = arrivalTimeMinute % 60
+
+    println("Время прибытия поезда: $arrivalHour:$arrivalMinute")
+}
