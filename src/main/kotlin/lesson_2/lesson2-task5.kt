f3a56dd6fb8e0167f5aa3e468e7349f9c5b36d17
PozyalovMihail <pozyalov@gmail.com>
2023-09-16 23:29:38 +0300

    KS-2-5 Task5 completed.

--- src/main/kotlin/lesson_2/lesson2-task5.kt
+++ src/main/kotlin/lesson_2/lesson2-task5.kt
@@ -1,2 +1,13 @@
 package lesson_2
+import kotlin.math.pow
+fun main() {
 
+    val money = 70000
+    val interestRate = 16.7
+    val time = 20
+    val compoundInterest = (1 + interestRate / 100)
+    val totalMoney = money * compoundInterest.pow(time)
+
+    print(String.format("%.3f", totalMoney))
+
+}
