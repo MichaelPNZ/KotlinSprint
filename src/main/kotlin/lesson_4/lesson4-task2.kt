e2a1389f9641d60d6a6a64ad6a1b3e346331b17a
PozyalovMihail <pozyalov@gmail.com>
2023-09-17 15:08:41 +0300

    KS-4-2 Task_2 completed.

--- src/main/kotlin/lesson_4/lesson4-task2.kt
+++ src/main/kotlin/lesson_4/lesson4-task2.kt
@@ -1,2 +1,24 @@
 package lesson_4
 
+fun main() {
+    val weightCargoOne = 20
+    val volumeCargoOne = 80
+
+    val weightCargoTwo = 50
+    val volumeCargoTwo = 100
+
+    println("Груз весом $weightCargoOne и объемом $volumeCargoOne соответствуем категории 'Average': ${
+        weightCargoOne >= minWeightAverage &&
+                weightCargoOne <= maxWeightAverage &&
+                volumeCargoOne < volumeAverage
+    }")
+    println("Груз весом $weightCargoTwo и объемом $volumeCargoTwo соответствуем категории 'Average': ${
+        weightCargoTwo >= minWeightAverage &&
+                weightCargoTwo <= maxWeightAverage &&
+                volumeCargoTwo < volumeAverage
+    }")
+}
+
+const val minWeightAverage = 35
+const val maxWeightAverage = 100
+const val volumeAverage = 100
