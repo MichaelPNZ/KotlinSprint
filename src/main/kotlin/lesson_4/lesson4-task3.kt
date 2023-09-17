beb07f0f5630b14a606fa4f5366ce856fbdaca57
PozyalovMihail <pozyalov@gmail.com>
2023-09-17 21:53:39 +0300

    KS-4-3 Task_3 completed.

--- src/main/kotlin/lesson_4/lesson4-task3.kt
+++ src/main/kotlin/lesson_4/lesson4-task3.kt
@@ -1,2 +1,20 @@
 package lesson_4
 
+fun main() {
+    val sun = true
+    val openTent = true
+    val humidity = 20
+    val season = "зима"
+
+    println("Благоприятные ли условия сейчас для роста бобовых? ${
+        (sun == SUN) &&
+                (openTent == OPEN_TENT) &&
+                (humidity == AIR_HUMIDITY) &&
+                (season != SEASON)
+    }")
+}
+
+const val SUN = true
+const val OPEN_TENT = true
+const val AIR_HUMIDITY = 20
+const val SEASON = "зима"
