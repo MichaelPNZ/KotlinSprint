7e75c4685f78a16d3e80f857b5f4868cb92d49b4
PozyalovMihail <pozyalov@gmail.com>
2023-09-17 14:11:00 +0300

    KS-3-5 Task_5 completed

--- src/main/kotlin/lesson_3/lesson3-task5.kt
+++ src/main/kotlin/lesson_3/lesson3-task5.kt
@@ -1,2 +1,13 @@
 package lesson_3
 
+fun main() {
+    var message = "D2-D4;0"
+
+    val whereFrom = "D2"
+    val where = "D4"
+    val numberMotion = 0
+
+    println(whereFrom)
+    println(where)
+    println(numberMotion)
+}
