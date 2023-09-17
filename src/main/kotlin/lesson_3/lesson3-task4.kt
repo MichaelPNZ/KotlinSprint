9d00061d1ba90c49a3a2dab59383a2ba3e782885
PozyalovMihail <pozyalov@gmail.com>
2023-09-17 14:03:19 +0300

    KS-3-4 Task_4 completed

--- src/main/kotlin/lesson_3/lesson3-task4.kt
+++ src/main/kotlin/lesson_3/lesson3-task4.kt
@@ -1,2 +1,15 @@
 package lesson_3
 
+fun main() {
+    var whereFrom = "E2"
+    var where = "E4"
+    var numberMotion = 1
+
+    var message = "$whereFrom-$where;ход $numberMotion"
+    println(message)
+    numberMotion += 1
+    whereFrom = "D2"
+    where = "D3"
+    message = "$whereFrom-$where;ход $numberMotion"
+    println(message)
+}
