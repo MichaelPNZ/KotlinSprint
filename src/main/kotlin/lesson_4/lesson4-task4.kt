ca5744dd9bf59b7c1f6c5fbde9394102ae2372f7
PozyalovMihail <pozyalov@gmail.com>
2023-09-17 23:14:31 +0300

    KS-4-4 Task_4 completed.

--- src/main/kotlin/lesson_4/lesson4-task4.kt
+++ src/main/kotlin/lesson_4/lesson4-task4.kt
@@ -1,2 +1,14 @@
 package lesson_4
 
+fun main() {
+    val day = 5
+    val typeProgram = day % 2
+
+    val program = """
+      Упражнения для рук: ${typeProgram != 0}
+      Упражнения для ног: ${typeProgram == 0}
+      Упражнения для спины: ${typeProgram == 0}
+      Упражнения для пресса: ${typeProgram != 0}
+    """.trimIndent()
+    println(program)
+}
