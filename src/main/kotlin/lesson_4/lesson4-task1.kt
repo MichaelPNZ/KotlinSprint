66a11827cd2efc415a90a974d32909d89b672bd2
PozyalovMihail <pozyalov@gmail.com>
2023-09-17 14:52:43 +0300

    KS-4-1 Task_1 completed.

--- src/main/kotlin/lesson_4/lesson4-task1.kt
+++ src/main/kotlin/lesson_4/lesson4-task1.kt
@@ -1,2 +1,10 @@
 package lesson_4
 
+fun main() {
+    val today = 13
+    val tomorrow = 4
+
+    println("Доступность столиков на сегодня: ${today < totalTables}. Доступность столиков на завтра: ${tomorrow < totalTables}.")
+}
+
+const val totalTables = 13
