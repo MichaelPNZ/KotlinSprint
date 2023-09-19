9c97de1f8e3f0567b28295abbc1d3335b9030658
PozyalovMihail <pozyalov@gmail.com>
2023-09-19 20:45:51 +0300

    KS-4-3 Task_3 fix name val.

--- src/main/kotlin/lesson_4/lesson4-task3.kt
+++ src/main/kotlin/lesson_4/lesson4-task3.kt
@@ -1,20 +1,20 @@
 package lesson_4
 
 fun main() {
-    val sun = true
-    val openTent = true
+    val isSunny = true
+    val isAwningOpen = true
     val humidity = 20
     val season = "зима"
 
     println("Благоприятные ли условия сейчас для роста бобовых? ${
-        (sun == SUN) &&
-                (openTent == OPEN_TENT) &&
+        (isSunny == IS_SUN) &&
+                (isAwningOpen == IS_AWNING_OPEN) &&
                 (humidity == AIR_HUMIDITY) &&
                 (season != SEASON)
     }")
 }
 
-const val SUN = true
-const val OPEN_TENT = true
+const val IS_SUN = true
+const val IS_AWNING_OPEN = true
 const val AIR_HUMIDITY = 20
 const val SEASON = "зима"
