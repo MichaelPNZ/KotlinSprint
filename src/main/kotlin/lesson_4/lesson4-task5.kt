9f5f6e72d4246a5d050f9655c509f7b38857e70b
PozyalovMihail <pozyalov@gmail.com>
2023-09-19 20:41:14 +0300

    KS-4-5 Task_5 completed.

--- src/main/kotlin/lesson_4/lesson4-task5.kt
+++ src/main/kotlin/lesson_4/lesson4-task5.kt
@@ -1,2 +1,25 @@
 package lesson_4
 
+fun main() {
+    print("наличие поврежденний корпуса: ")
+    val damage = readLine().toBoolean()
+    print("текущий состав экипажа: ")
+    val crewNumber = readLine()?.toInt()
+    print("количество ящиков с провизией на борту: ")
+    val wireBoxes = readln().toInt()
+    print("благоприятность метеоусловий: ")
+    val  weather = readLine().toBoolean()
+
+    var result = false
+
+    if ((damage == false) && (crewNumber in "55".toInt().."70".toInt()) && (wireBoxes >= 50) && (weather == true || false)) {
+        result = true
+    }
+
+    if ((damage == true) && (crewNumber == 70) && (weather == true) && (wireBoxes > 50)) {
+        result = true
+    }
+
+    println("Корабля может приступать к долгосрочному плаванию: $result")
+}
+
