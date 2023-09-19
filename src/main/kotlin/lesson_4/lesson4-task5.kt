e213cbf07c5077ea0a1f7971b28d97d0696381fe
PozyalovMihail <pozyalov@gmail.com>
2023-09-19 20:48:16 +0300

    KS-4-5 Task_5 fix name val.

--- src/main/kotlin/lesson_4/lesson4-task5.kt
+++ src/main/kotlin/lesson_4/lesson4-task5.kt
@@ -2,21 +2,21 @@ package lesson_4
 
 fun main() {
     print("наличие поврежденний корпуса: ")
-    val damage = readLine().toBoolean()
+    val isDamage = readLine().toBoolean()
     print("текущий состав экипажа: ")
     val crewNumber = readLine()?.toInt()
     print("количество ящиков с провизией на борту: ")
     val wireBoxes = readln().toInt()
     print("благоприятность метеоусловий: ")
-    val  weather = readLine().toBoolean()
+    val  isClean = readLine().toBoolean()
 
     var result = false
 
-    if ((damage == false) && (crewNumber in "55".toInt().."70".toInt()) && (wireBoxes >= 50) && (weather == true || false)) {
+    if ((isDamage == false) && (crewNumber in "55".toInt().."70".toInt()) && (wireBoxes >= 50) && (isClean == true || false)) {
         result = true
     }
 
-    if ((damage == true) && (crewNumber == 70) && (weather == true) && (wireBoxes > 50)) {
+    if ((isDamage == true) && (crewNumber == 70) && (isClean == true) && (wireBoxes > 50)) {
         result = true
     }
 
