311d2a6dcea46948ddb5be6bc206d8f38bec1eb5
PozyalovMihail <pozyalov@gmail.com>
2023-09-29 13:22:53 +0300

    KS-4-4 Task_4 fix name val.

--- src/main/kotlin/lesson_4/lesson4-task4.kt
+++ src/main/kotlin/lesson_4/lesson4-task4.kt
@@ -2,13 +2,17 @@ package lesson_4
 
 fun main() {
     val day = 5
-    val typeProgram = day % 2
+    val isEven = day % 2
+    val hands = 1
+    val legs = 2
+    val back = 2
+    val press = 1
 
     val program = """
-      Упражнения для рук: ${typeProgram != 0}
-      Упражнения для ног: ${typeProgram == 0}
-      Упражнения для спины: ${typeProgram == 0}
-      Упражнения для пресса: ${typeProgram != 0}
+      Упражнения для рук: ${isEven == hands}
+      Упражнения для ног: ${isEven == legs}
+      Упражнения для спины: ${isEven == back}
+      Упражнения для пресса: ${isEven == press}
     """.trimIndent()
 
     println(program)
