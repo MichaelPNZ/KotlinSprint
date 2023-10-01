594c4040bc6852e1fef690383f3efab52e02fc52
PozyalovMihail <pozyalov@gmail.com>
2023-10-01 21:03:34 +0300

    KS-5-5 Task_5 completed.

--- src/main/kotlin/lesson_5/lesson5-task5.kt
+++ src/main/kotlin/lesson_5/lesson5-task5.kt
@@ -1,2 +1,24 @@
 package lesson_5
 
+fun main() {
+
+    val firstRandomNumber = (1..100).random()
+    val secondRandomNumber = (1..100).random()
+
+    println("Отгадайте два числа от 1 до 100")
+    println("Введите первое число: ")
+    val firstNumber = readLine()!!.toInt()
+    println("Введите второе число: ")
+    val secondNumber = readLine()!!.toInt()
+    val winNumber = "Выигрышные числа: $firstRandomNumber и $secondRandomNumber"
+
+    if ((firstNumber == firstRandomNumber) && (secondNumber == secondRandomNumber)) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if ((firstNumber == firstRandomNumber) || (secondNumber == secondRandomNumber)) {
+        println("Вы выиграли утешительный приз!")
+        println(winNumber)
+    } else {
+        println("Неудача! Крутите барабан!")
+        println(winNumber)
+    }
+}
