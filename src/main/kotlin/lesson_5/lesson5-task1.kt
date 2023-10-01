e60a9355c2a5117216f86b53e2fc881728fca7ae
PozyalovMihail <pozyalov@gmail.com>
2023-10-01 15:33:22 +0300

    KS-5-1 Task_1 completed.

--- src/main/kotlin/lesson_5/lesson5-task1.kt
+++ src/main/kotlin/lesson_5/lesson5-task1.kt
@@ -1,2 +1,12 @@
 package lesson_5
 
+fun main() {
+
+    print("Сложи 5+7=")
+    val botCheck = readLine()!!.toInt()
+
+    if (botCheck == TRUE_RESULT_BOT_CHECK) { println("Добро пожаловать!") }
+    else { println("Доступ запрещен.") }
+}
+
+const val TRUE_RESULT_BOT_CHECK = 12
