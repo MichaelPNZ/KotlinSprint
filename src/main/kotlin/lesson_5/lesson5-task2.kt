c018c8962b2307b5bf0953b78044081987736d38
PozyalovMihail <pozyalov@gmail.com>
2023-10-01 20:01:19 +0300

    KS-5-2 Task_2 completed.

--- src/main/kotlin/lesson_5/lesson5-task2.kt
+++ src/main/kotlin/lesson_5/lesson5-task2.kt
@@ -1,2 +1,20 @@
 package lesson_5
 
+import java.time.LocalDate
+
+fun main() {
+
+    println("Введите год своего рождения: ")
+    val userAge = readLine()!!.toInt()
+    val currentDate = LocalDate.now().year
+    val currentAge = currentDate - userAge
+
+    val resultText = if (currentAge >= AGE_OF_MAJORITY) "Показать экран со скрытым контентом"
+    else if (userAge == 16 || userAge == 17) "Показать экран с ограниченным контентом"
+    else "Вернуться на главный экран приложения"
+
+    println(resultText)
+}
+
+const val AGE_OF_MAJORITY = 18
+
