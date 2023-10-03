bebbdac975ca174cb4d34d434b2fa744141afbfb
PozyalovMihail <pozyalov@gmail.com>
2023-10-03 12:49:05 +0300

    KS-6-4 Task_4 completed.

--- src/main/kotlin/lesson_6/lesson6-task4.kt
+++ src/main/kotlin/lesson_6/lesson6-task4.kt
@@ -1,2 +1,23 @@
 package lesson_6
 
+fun main() {
+    val number = (1..9).random()
+    var attempts = 5
+    println("Угадайте число!")
+
+    while (attempts > 0) {
+        attempts--
+        println("Введите свое число: ")
+        val playerNumber = readLine()!!.toInt()
+
+        if (playerNumber == number) {
+            attempts = 0
+            println("Это была великолепная игра!")
+        } else if (attempts == 0) {
+            println("Неверно! Остальсь попыток: ${attempts}")
+            println("Было загадано число ${number}")
+        } else {
+                println("Неверно! Остальсь попыток: ${attempts}")
+        }
+    }
+}
