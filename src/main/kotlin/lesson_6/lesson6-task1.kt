b6e3618757f02bc52d3ce45de28a80dde993ce99
PozyalovMihail <pozyalov@gmail.com>
2023-10-03 11:48:00 +0300

    KS-6-1 Task_1 completed.

--- src/main/kotlin/lesson_6/lesson6-task1.kt
+++ src/main/kotlin/lesson_6/lesson6-task1.kt
@@ -1,2 +1,30 @@
 package lesson_6
 
+fun main() {
+    var authorization = false
+
+    var login = ""
+    var password = ""
+
+    while (authorization == false) {
+        if (login == "" && password == "") {
+            println("Создайте логин: ")
+            login = readLine().toString()
+            println("Создайте пароль: ")
+            password = readLine().toString()
+        } else {
+            println("Введите логин: ")
+            val inputLogin = readLine().toString()
+            println("Введите пароль: ")
+            val inputPassword = readLine().toString()
+
+            if ((inputLogin == login) && (inputPassword == password)) {
+                authorization = true
+                println("Авторизация прошла успешно")
+            } else {
+                println("Логин и пароль не верны!")
+            }
+        }
+    }
+}
+
