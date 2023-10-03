c0d10b96ac08db90df201d6350d04ef4bb1a667f
PozyalovMihail <pozyalov@gmail.com>
2023-10-03 21:17:34 +0300

    KS-4-4 Task_4 fix isEven logic.

--- src/main/kotlin/lesson_4/lesson4-task4.kt
+++ src/main/kotlin/lesson_4/lesson4-task4.kt
@@ -2,17 +2,17 @@ package lesson_4
 
 fun main() {
     val day = 5
-    val isEven = day % 2
-    val hands = 1
-    val legs = 2
-    val back = 2
-    val press = 1
+    val isEven = day % 2 == 0
+    val hands = !isEven
+    val legs = isEven
+    val back = isEven
+    val press = !isEven
 
     val program = """
-      Упражнения для рук: ${isEven == hands}
-      Упражнения для ног: ${isEven == legs}
-      Упражнения для спины: ${isEven == back}
-      Упражнения для пресса: ${isEven == press}
+      Упражнения для рук: ${hands}
+      Упражнения для ног: ${legs}
+      Упражнения для спины: ${back}
+      Упражнения для пресса: ${press}
     """.trimIndent()
 
     println(program)
