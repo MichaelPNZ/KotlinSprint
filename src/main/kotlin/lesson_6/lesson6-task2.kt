c5be112f6960c7af0cb3d2225cf9603821d4722e
PozyalovMihail <pozyalov@gmail.com>
2023-10-03 12:03:33 +0300

    KS-6-2 Task_2 completed.

--- src/main/kotlin/lesson_6/lesson6-task2.kt
+++ src/main/kotlin/lesson_6/lesson6-task2.kt
@@ -1,2 +1,17 @@
 package lesson_6
 
+fun main() {
+
+    println("Введите количество секунд: ")
+    val seconds = readLine()!!.toInt()
+    var timer = seconds
+
+    while (timer > 0) {
+        timer--
+        Thread.sleep(1000)
+
+        if (timer == 0) {
+            println("Прошло ${seconds} секунд")
+        }
+    }
+}
