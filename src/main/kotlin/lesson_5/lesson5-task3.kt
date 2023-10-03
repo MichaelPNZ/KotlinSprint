4e5dcfd45a25cef93661742f094088c6cacd613d
PozyalovMihail <pozyalov@gmail.com>
2023-10-03 21:50:20 +0300

    KS-5-3 Task_3 fix.

--- src/main/kotlin/lesson_5/lesson5-task3.kt
+++ src/main/kotlin/lesson_5/lesson5-task3.kt
@@ -3,12 +3,13 @@ package lesson_5
 fun main() {
     println("Отгадайте два числа от 1 до 100")
     println("Введите первое число: ")
-    val firstNumber = readLine()!!.toInt()
+    val firstNumber = readln().toInt()
     println("Введите второе число: ")
-    val secondNumber = readLine()!!.toInt()
+    val secondNumber = readln().toInt()
     val winNumber = "Выигрышные числа: $FIRST_NUMBER и $SECOND_NUMBER"
 
-    if ((firstNumber == FIRST_NUMBER) && (secondNumber == SECOND_NUMBER)) {
+    if (((firstNumber == FIRST_NUMBER) && (secondNumber == SECOND_NUMBER))
+        || ((firstNumber == SECOND_NUMBER) && (secondNumber == FIRST_NUMBER))) {
         println("Поздравляем! Вы выиграли главный приз!")
     } else if ((firstNumber == FIRST_NUMBER) || (secondNumber == SECOND_NUMBER)) {
         println("Вы выиграли утешительный приз!")
