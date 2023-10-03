4caaba3308e4783290cfe6808c0596fdac083e91
PozyalovMihail <pozyalov@gmail.com>
2023-10-03 12:08:06 +0300

    KS-6-3 Task_3 completed.

--- src/main/kotlin/lesson_6/lesson6-task3.kt
+++ src/main/kotlin/lesson_6/lesson6-task3.kt
@@ -1,2 +1,18 @@
 package lesson_6
 
+fun main() {
+
+    println("Введите количество секунд: ")
+    val seconds = readLine()!!.toInt()
+    var timer = seconds
+
+    while (timer > 0) {
+        println("Осталось секунд: ${timer--}")
+        Thread.sleep(1000)
+
+        if (timer == 0) {
+            println("Время вышло")
+        }
+    }
+
+}
