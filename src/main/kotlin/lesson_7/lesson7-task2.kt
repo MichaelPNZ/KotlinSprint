5adb1f0fa44163ee4414973c8670a1ba64eebe13
PozyalovMihail <pozyalov@gmail.com>
2023-10-04 11:01:37 +0300

    KS-7-2 Task_2 completed.

--- src/main/kotlin/lesson_7/lesson7-task2.kt
+++ src/main/kotlin/lesson_7/lesson7-task2.kt
@@ -1,2 +1,20 @@
 package lesson_7
 
+fun main() {
+    var authorization = false
+
+    while (authorization == false) {
+        val randomNumber = (1000..9999).random()
+        println("Ваш код авторизации: ${randomNumber}")
+        println("Введите полученный код: ")
+        val code = readln().toInt()
+
+        when {
+            code == randomNumber -> {
+                println("Добра пожаловать!")
+                authorization = true
+            }
+            else -> println("Код введен неверно.")
+        }
+    }
+}
