b9cbc4591bdfb869b6d3cc1cbfe981a20839d164
PozyalovMihail <pozyalov@gmail.com>
2023-10-04 11:29:38 +0300

    KS-7-4 Task_4 completed.

--- src/main/kotlin/lesson_7/lesson7-task4.kt
+++ src/main/kotlin/lesson_7/lesson7-task4.kt
@@ -1,2 +1,15 @@
 package lesson_7
 
+fun main() {
+    println("На сколько секунд поставить таймер: ")
+    val timer = readln().toInt()
+
+    for (second in timer downTo 0) {
+        Thread.sleep(1000)
+
+        when {
+            second != 0 -> println("Осталось: ${second} секунд.")
+            second == 0 -> println("Время вышло.")
+        }
+    }
+}
