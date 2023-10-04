d79c66ddecdedca4f0fb5fe506ddcd05fdcae44e
PozyalovMihail <pozyalov@gmail.com>
2023-10-04 11:18:05 +0300

    KS-7-3 Task_3 completed.

--- src/main/kotlin/lesson_7/lesson7-task3.kt
+++ src/main/kotlin/lesson_7/lesson7-task3.kt
@@ -1,2 +1,16 @@
 package lesson_7
 
+fun main() {
+    println("Введите число: ")
+    val number = readln().toInt()
+
+    for (even in 0..number) {
+        Thread.sleep(500)
+        if (even % 2 == 0) {
+            if (even == 0) {
+                continue
+            }
+            println(even)
+        }
+    }
+}
