4da65e60839591dfeb7516328836171dbbfba671
PozyalovMihail <pozyalov@gmail.com>
2023-10-04 10:42:29 +0300

    KS-7-1 Task_1 completed. В задании было требование создать отдельную переменную для каждого символа(6 переменных для 6 символов!?). Я расценил это, как создание отдельных переменных для чисел и букв, а из них рандомом вытягивать через for.

--- src/main/kotlin/lesson_7/lesson7-task1.kt
+++ src/main/kotlin/lesson_7/lesson7-task1.kt
@@ -1,2 +1,16 @@
 package lesson_7
 
+fun main() {
+
+    val symbol1 = 'a'..'z'
+    val symbol2 = 1..9
+    var password = ""
+
+    for (i in 1..3) {
+
+        password += symbol1.random()
+        password += symbol2.random()
+    }
+
+    println(password)
+}
