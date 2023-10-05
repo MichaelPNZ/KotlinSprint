d92af055f04b3ef29a55b0c761a2d2d89276a42e
PozyalovMihail <pozyalov@gmail.com>
2023-10-05 13:01:00 +0300

    KS-8-2 Task_2 completed.

--- src/main/kotlin/lesson_8/lesson8-task2.kt
+++ src/main/kotlin/lesson_8/lesson8-task2.kt
@@ -1,2 +1,18 @@
 package lesson_8
 
+fun main() {
+    val arrayOfIngredients = arrayOf("квас", "картошка", "яйцо", "лук", "огурец", "колбаса", "сметана", "зелень")
+
+    println("Введите ингредиент: ")
+    val ingredient = readLine().toString()
+    var equal = false
+
+    for (i in arrayOfIngredients) {
+        if (i == ingredient) { equal = true }
+    }
+
+    when(equal) {
+        true -> println("Ингредиент ${ingredient} в рецепте есть")
+        else -> println("Такого ингредиента в рецепте нет")
+    }
+}
