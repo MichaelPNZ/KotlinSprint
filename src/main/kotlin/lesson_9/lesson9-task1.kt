5fe5958975dc3fbc5806d71e9c38dd8627cadaa7
PozyalovMihail <pozyalov@gmail.com>
2023-10-06 11:44:12 +0300

    KS-9-1 Task_1 completed.

--- src/main/kotlin/lesson_9/lesson9-task1.kt
+++ src/main/kotlin/lesson_9/lesson9-task1.kt
@@ -1,7 +1,7 @@
 package lesson_9
 
 fun main() {
-    val arrayOfIngredients = mutableListOf("перец","яйцо","сыр","картофель","майонез","кукуруза","лук")
+    val arrayOfIngredients = listOf("перец","яйцо","сыр","картофель","майонез","кукуруза","лук")
     println("В рецепте есть следующие ингредиенты:")
     arrayOfIngredients.forEach { println(it) }
 }
