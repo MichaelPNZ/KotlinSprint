0420b3e6d71b4b06097643e516bc9316cf0b3c7a
PozyalovMihail <pozyalov@gmail.com>
2023-10-06 09:57:32 +0300

    KS-6-3 Task_3 fix.

--- src/main/kotlin/lesson_6/lesson6-task3.kt
+++ src/main/kotlin/lesson_6/lesson6-task3.kt
@@ -9,10 +9,6 @@ fun main() {
     while (timer > 0) {
         println("Осталось секунд: ${timer--}")
         Thread.sleep(1000)
-
-        if (timer == 0) {
-            println("Время вышло")
-        }
     }
-
+    println("Время вышло")
 }
