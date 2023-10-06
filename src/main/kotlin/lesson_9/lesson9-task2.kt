c40a99cdeedf424c3362b541e95102248ab5f818
PozyalovMihail <pozyalov@gmail.com>
2023-10-06 11:58:46 +0300

    KS-9-2 Task_2 completed.

--- src/main/kotlin/lesson_9/lesson9-task2.kt
+++ src/main/kotlin/lesson_9/lesson9-task2.kt
@@ -1,2 +1,21 @@
 package lesson_9
 
+fun main() {
+    val arrayOfIngredients = mutableListOf("Лук", "Яйцо", "Колбаса")
+    println("В рецепте есть базовые ингредиенты: ${arrayOfIngredients.joinToString()}")
+    println("Желаете добавить еще?")
+    val answer = readln()
+
+   for (i in arrayOfIngredients) {
+       when {
+           answer == "нет" -> break
+           else -> {
+               println("Какой ингредиент вы хотите добавить?")
+               val newIngredient = readln()
+               arrayOfIngredients.add(newIngredient)
+               println("Теперь в рецепте есть следующие ингредиенты: ${arrayOfIngredients.joinToString()}")
+               break
+           }
+       }
+   }
+}
