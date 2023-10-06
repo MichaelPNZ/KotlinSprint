d5cc6e2e974bb245ab107dd29245a17eb84f786b
PozyalovMihail <pozyalov@gmail.com>
2023-10-06 13:03:53 +0300

    KS-9-4 Task_4 completed.

--- src/main/kotlin/lesson_9/lesson9-task4.kt
+++ src/main/kotlin/lesson_9/lesson9-task4.kt
@@ -1,2 +1,10 @@
 package lesson_9
 
+fun main() {
+    println("Введите 5 ингредиентов через запятую:")
+    val ingredient = readln()
+    val arrayOfIngredients = ingredient.split(",").toList()
+
+    val sortedArray = arrayOfIngredients.sorted()
+    println(sortedArray.joinToString())
+}
