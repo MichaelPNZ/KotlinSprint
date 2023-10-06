429ee9852333e72adf735c9d29ee41e849c5ac86
PozyalovMihail <pozyalov@gmail.com>
2023-10-06 10:03:05 +0300

    KS-6-5 Task_5 fix.

--- src/main/kotlin/lesson_6/lesson6-task5.kt
+++ src/main/kotlin/lesson_6/lesson6-task5.kt
@@ -3,17 +3,16 @@ package lesson_6
 fun main() {
     var attempts = 3
 
-    while (attempts > 0) {
+    while (attempts-- > 0) {
         val numberOne = (1..9).random()
         val numberTwo = (1..9).random()
         println("Докажи, что ты не бот! Реши пример: ${numberOne}+${numberTwo}=")
         val result = readln().toInt()
-        attempts--
 
         when {
             result == (numberOne + numberTwo) -> {
-                attempts = 0
                 println("Добро пожаловать!")
+                break
                 }
             attempts == 0 -> println("Доступ запрещен")
             else -> println("Неверно. Осталось попыток: ${attempts}")
