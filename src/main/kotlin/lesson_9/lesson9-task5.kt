7c6d57bca1ce582fc4c04c1bf9f5bbaaf531b17e
PozyalovMihail <pozyalov@gmail.com>
2023-10-06 23:20:15 +0300

    KS-9-5 Task_5 Частично сделано.

--- src/main/kotlin/lesson_9/lesson9-task5.kt
+++ src/main/kotlin/lesson_9/lesson9-task5.kt
@@ -1,2 +1,20 @@
 package lesson_9
 
+fun main() {
+    val array = mutableListOf<String>()
+    println("Введите 5 ингредиентов")
+
+    for (i in 1..5) {
+        println("Ингредиент ${i}: ")
+        val ingredient = readln()
+        if (array.contains(ingredient)) { continue }
+        else { array.add(ingredient) }
+    }
+
+    val separator = ","
+    val sortedArray = array.sorted().joinToString(separator)
+
+    sortedArray.forEach {
+        print("${it}")
+    }
+}
