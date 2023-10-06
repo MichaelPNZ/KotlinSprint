7217ff2b68153a4b8ef01f13858d6767a6a81424
PozyalovMihail <pozyalov@gmail.com>
2023-10-06 10:56:41 +0300

    KS-8-4 Task_4 completed.

--- src/main/kotlin/lesson_8/lesson8-task4.kt
+++ src/main/kotlin/lesson_8/lesson8-task4.kt
@@ -1,2 +1,21 @@
 package lesson_8
 
+fun main() {
+    val arrayOfIngredients = mutableListOf("перец","яйцо","сыр","картофель","майонез","кукуруза","лук")
+    println("Список ингредиентов: ${arrayOfIngredients.joinToString()}")
+
+    println("Какой ингредиент вы хотите заменить?")
+    val changedIngredient = readln()
+
+    if (arrayOfIngredients.contains(changedIngredient)) {
+        arrayOfIngredients.remove(changedIngredient)
+
+        println("Какой ингредиент добавыть?")
+        val newIngredient = readln()
+        arrayOfIngredients.add(newIngredient)
+
+        println("Готово! Вы сохранили следующий список: ${arrayOfIngredients.joinToString()}")
+    } else {
+        println("Такого ингредиента нет в списке!")
+    }
+}
