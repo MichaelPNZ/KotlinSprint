ac293a639cd84733dcf720dc0f867d0f70477587
PozyalovMihail <pozyalov@gmail.com>
2023-10-07 16:02:36 +0300

    KS-7-5 Task_5 fix.

--- src/main/kotlin/lesson_5/lesson5-task5.kt
+++ src/main/kotlin/lesson_5/lesson5-task5.kt
@@ -1,17 +1,1 @@
-package lesson_5
-
-
-fun getRandomString(length: Int) : String {
-    val charset = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-    return (1..length)
-        .map { charset.random() }
-        .joinToString("")
-}
-fun main() {
-    println("Укажите длину пароля: ")
-    val longPassword = readln().toInt()
-
-    val password = getRandomString(longPassword)
-
-    println(password)
-}
+package lesson_7
