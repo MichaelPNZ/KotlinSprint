465587d07c5f820fe1565b65cf9834af884aaaf8
PozyalovMihail <pozyalov@gmail.com>
2023-10-08 15:15:15 +0300

    KS-7-3 Task_3 fix.

--- src/main/kotlin/lesson_7/lesson7-task3.kt
+++ src/main/kotlin/lesson_7/lesson7-task3.kt
@@ -4,13 +4,9 @@ fun main() {
     println("Введите число: ")
     val number = readln().toInt()
 
-    for (even in 0..number) {
+    for (even in 0..number step 2) {
         Thread.sleep(500)
-        if (even % 2 == 0) {
-            if (even == 0) {
-                continue
-            }
-            println(even)
-        }
+        if (even == 0) continue
+        println(even)
     }
 }
