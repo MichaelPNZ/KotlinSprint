03d66ac465f78bdf82b12e95cca3826d2a97e7af
PozyalovMihail <pozyalov@gmail.com>
2023-10-09 21:44:19 +0300

    KS-8-3 Task_3 fix.

--- src/main/kotlin/lesson_8/lesson8-task3.kt
+++ src/main/kotlin/lesson_8/lesson8-task3.kt
@@ -4,7 +4,7 @@ fun main() {
     val arrayOfIngredients = arrayOf("квас", "картошка", "яйцо", "лук", "огурец", "колбаса", "сметана", "зелень")
 
     println("Введите ингредиент: ")
-    val ingredient = readLine().toString()
+    val ingredient = readln()
 
     if (arrayOfIngredients.contains(ingredient)) println("Ингредиент ${ingredient} в рецепте есть")
     else println("Такого ингредиента в рецепте нет")
