3b538fadfd419bf522582f9021eff55b69bfb4c3
PozyalovMihail <pozyalov@gmail.com>
2023-10-10 13:10:26 +0300

    KS-10-3 Task_3 completed.

--- src/main/kotlin/lesson_10/lesson10-task3.kt
+++ src/main/kotlin/lesson_10/lesson10-task3.kt
@@ -1,2 +1,19 @@
 package lesson_10
 
+fun main() {
+    println("Укажите длину пароля: ")
+    val longPassword = readln().toInt()
+    val password = getRandomPassword(longPassword)
+    println(password)
+}
+
+fun getRandomPassword(length: Int) : String {
+    val symbol1 = "!\"\\#\$%&'()*+,-./ "
+    val symbol2 = '0'..'9'
+    var password = ""
+    for (i in 1..length / 2) {
+        password += symbol1.random()
+        password += symbol2.random()
+    }
+    return password
+}
