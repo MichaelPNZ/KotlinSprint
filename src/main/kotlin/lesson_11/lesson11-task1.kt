ecc189631df5512c3c1b65b57adf75d6f7011b3b
PozyalovMihail <pozyalov@gmail.com>
2023-10-11 11:51:51 +0300

    KS-11-1 Task_1 completed.

--- src/main/kotlin/lesson_11/lesson11-task1.kt
+++ src/main/kotlin/lesson_11/lesson11-task1.kt
@@ -1,2 +1,34 @@
 package lesson_11
 
+
+class User (
+    val id: Int,
+    val login: String,
+    val password: String,
+    val email: String,
+) {}
+
+fun main() {
+    val userOne = User(
+        id = 1,
+        login = "qwerty",
+        password = "asdfg",
+        email = "zxcvb@gmail.com",
+    )
+
+    val userTwo = User(
+        id = 2,
+        login = "uiopsgs",
+        password = "hjklffhd",
+        email = "nmvbcn@gmail.com",
+    )
+    println(userOne.id)
+    println(userOne.login)
+    println(userOne.password)
+    println(userOne.email)
+    println()
+    println(userTwo.id)
+    println(userTwo.login)
+    println(userTwo.password)
+    println(userTwo.email)
+}
