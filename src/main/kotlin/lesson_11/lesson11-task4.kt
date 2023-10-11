4abbadb9f089bf4058aeca9cf7b8d10d777ba7fe
PozyalovMihail <pozyalov@gmail.com>
2023-10-11 15:40:42 +0300

    KS-11-4 Task_4 completed.

--- src/main/kotlin/lesson_11/lesson11-task4.kt
+++ src/main/kotlin/lesson_11/lesson11-task4.kt
@@ -1,2 +1,72 @@
 package lesson_11
 
+class Contact(
+    val image: String,
+    val firstName: String,
+    val secondName: String,
+    val iconButton: List<String>,
+    val nameButton: List<String>,
+    val phone: List<String>,
+    val phoneNumber: List<String>,
+    val faceTime: String,
+    val iCloud: String,
+    val emailICloud: String,
+    val friendName: List<String>,
+    val friendNickName: List<String>,
+) {
+    fun printUserInfo() {
+        println(image)
+        println("${firstName} ${secondName}")
+    }
+
+    fun printButtons() { forEach(iconButton, nameButton) }
+
+    fun printPhoneNumber() { forEach(phone, phoneNumber) }
+
+    fun printFaceTime() { println("${faceTime} ${iconButton[2]}  ${iconButton[1]}") }
+
+    fun printICloud() {
+        println(iCloud)
+        println(emailICloud)
+    }
+
+    fun printFriends() { forEach(friendName, friendNickName) }
+
+    fun forEach(string: List<String>, string2: List<String>) {
+        var index = 0
+        for (i in 1..string.size) {
+            println(string[index])
+            println(string2[index])
+            index += 1
+        }
+    }
+}
+
+fun main() {
+    val contact = Contact(
+        image = "MouseImage",
+        firstName= "First Name",
+        secondName= "Second Name",
+        iconButton = listOf("MassageIcon","PhoneCallingIcon","VideoCallingIcon","EmailIcon"),
+        nameButton = listOf("написать","вызов","видео","почта"),
+        phone = listOf("сотовый","домашний"),
+        phoneNumber = listOf("8 (999) 999-42-42","8 (999) 888-42-42"),
+        faceTime = "FaceTime",
+        iCloud = "ICloud",
+        emailICloud = "mail@mail.ru",
+        friendName = listOf("жена","подруга","друг"),
+        friendNickName = listOf("Катя","Маша","Женя"),
+        )
+
+    contact.printUserInfo()
+    println()
+    contact.printButtons()
+    println()
+    contact.printPhoneNumber()
+    println()
+    contact.printFaceTime()
+    println()
+    contact.printICloud()
+    println()
+    contact.printFriends()
+}
