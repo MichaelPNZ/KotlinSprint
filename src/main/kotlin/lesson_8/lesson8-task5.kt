bdcf101e82c407d14b432324732a61348a187cb0
PozyalovMihail <pozyalov@gmail.com>
2023-10-11 10:58:46 +0300

    KS-8-5 Task_5 fix.

--- src/main/kotlin/lesson_8/lesson8-task5.kt
+++ src/main/kotlin/lesson_8/lesson8-task5.kt
@@ -1,14 +1,14 @@
 package lesson_8
 
 fun main() {
-    val arrayOfIngredients = mutableListOf<String>()
+    var arrayOfIngredients = arrayOf<String>()
     println("Сколько ингредиентов вы хотите добавить в рецепт?")
     val count = readln().toInt()
 
     for (i in 1..count) {
-        println("Введите название ингредиента:")
+        println("Введите название ингредиента №${i}:")
         val ingredient = readln()
-        arrayOfIngredients.add(ingredient)
+        arrayOfIngredients += ingredient
     }
     println("Список ваших ингредиентов: ${ arrayOfIngredients.joinToString() }.")
 }
