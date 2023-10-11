8fdbfa5d8eb6ddbb6b4d585db8f250eb29799bb9
PozyalovMihail <pozyalov@gmail.com>
2023-10-11 13:58:50 +0300

    KS-11-3 Task_3 completed. Что-то я плохо понял, что от меня требуется=)

--- src/main/kotlin/lesson_11/lesson11-task3.kt
+++ src/main/kotlin/lesson_11/lesson11-task3.kt
@@ -1,2 +1,62 @@
 package lesson_11
 
+class Room(
+    val id: Int,
+    val coverRoom: String,
+    val nameRoom: String,
+    val participantList: List<String>,
+    val userName: String,
+    var status: String,
+) {
+    fun longPressed() {
+        println("Было долгое нажатие")
+        println(userName)
+    }
+
+    fun testPrint() {
+        println("-------------------")
+        println(id)
+        println(coverRoom)
+        println(nameRoom)
+        println(participantList)
+        println(userName)
+        println(status)
+        println("-------------------")
+    }
+}
+
+fun main() {
+    val room1 = Room(
+        id = 1,
+        coverRoom = "Картанка бабочки",
+        nameRoom = "бабочки",
+        participantList = arrayListOf("Petya", "Kolya"),
+        userName = "Petya",
+        status = "пользователь заглушен"
+    )
+
+    val room2 = Room(
+        id = 2,
+        coverRoom = "Картанка колеса",
+        nameRoom = "обсуждение колес",
+        participantList = arrayListOf("Petya", "Kolya"),
+        userName = "Kolya",
+        status = "разговаривает"
+    )
+
+    val room3 = Room(
+        id = 3,
+        coverRoom = "Картанка монитора",
+        nameRoom = "мониторы тут",
+        participantList = arrayListOf("Petya", "Kolya", "Tolya"),
+        userName = "Tolya",
+        status = "микрофон выключен"
+    )
+
+    room1.testPrint()
+    room1.longPressed()
+    room2.testPrint()
+    room2.longPressed()
+    room3.testPrint()
+    room3.longPressed()
+}
