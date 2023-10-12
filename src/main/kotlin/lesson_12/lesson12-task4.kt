76b36e74164488f38dee250c058fc3bf4addf35f
PozyalovMihail <pozyalov@gmail.com>
2023-10-12 09:53:03 +0300

    KS-12-4 Task_4 completed.

--- src/main/kotlin/lesson_12/lesson12-task4.kt
+++ src/main/kotlin/lesson_12/lesson12-task4.kt
@@ -1,2 +1,23 @@
 package lesson_12
 
+class Weather3(val temperatureOfDay: Int, val temperatureOfNight: Int, val isRain: Boolean = false, val pressure: Int) {
+
+//    constructor(
+//        temperatureOfDay: Int,
+//        temperatureOfNight: Int,
+//        isRain: Boolean,
+//        pressure: Int,
+//    ) : this(temperatureOfDay, temperatureOfNight, isRain, pressure) {
+//
+//    }
+    init {
+        println("температура днем: ${temperatureOfDay}, температура ночью: ${temperatureOfNight}, давление: ${pressure}, дождь: ${isRain}.")
+    }
+}
+
+fun main() {
+    val weather = Weather3(temperatureOfDay = 28, temperatureOfNight = 15, isRain = true, pressure = 770)
+    println(weather.isRain)
+    val weather2 = Weather3(temperatureOfDay = 5, temperatureOfNight = -10, pressure = 750)
+    println(weather2.isRain)
+}
