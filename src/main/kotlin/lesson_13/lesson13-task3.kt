b3f55f84a089dadbbb229ae0bdb3d848c37517e7
PozyalovMihail <pozyalov@gmail.com>
2023-10-12 12:47:54 +0300

    KS-13-3 Task_3 completed.

--- src/main/kotlin/lesson_13/lesson13-task3.kt
+++ src/main/kotlin/lesson_13/lesson13-task3.kt
@@ -1,2 +1,15 @@
 package lesson_13
 
+class PhoneBook3(val name: String, val phoneNumber: Long, val company: String?) {
+    fun print() { println("-Имя: ${name}\n-Номер: ${phoneNumber}\n-Компания: ${company}") }
+}
+
+fun main() {
+    val list = listOf(PhoneBook3("Michael", 8999888471, "Adidas"),
+        PhoneBook3("Timofey", 89998887799, null ?: "<не указано>"),
+        PhoneBook3("Janna", 89998887854, "null"),
+        PhoneBook3("Katrin", 89998887547, "Rostics"),
+        PhoneBook3("Rafik", 89998887235, null ?: "<не указано>"))
+
+    list.forEach() { it.print() }
+}
