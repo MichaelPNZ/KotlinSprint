5ce15b64e00d5a7316c572da9ec37215c4fc9343
PozyalovMihail <pozyalov@gmail.com>
2023-10-12 16:24:51 +0300

    KS-13-5 Task_5 completed.

--- src/main/kotlin/lesson_13/lesson13-task5.kt
+++ src/main/kotlin/lesson_13/lesson13-task5.kt
@@ -1,2 +1,17 @@
 package lesson_13
 
+class PhoneBook5(val name: String, val phoneNumber: Long?, val company: String?) {
+    fun print() { println("-Имя: ${name}\n-Номер: ${phoneNumber}\n-Компания: ${company}") }
+}
+
+fun main() {
+
+    println("Введите номер телефона(только цифры)")
+    try {
+        val phoneNumber = readln().toLong()
+        val contact = PhoneBook5(name = "Petr", phoneNumber, company = "Pepsi")
+        contact.print()
+    } catch (e: Exception) {
+        println("Вы ввели:  ${e.message}")
+    }
+}
