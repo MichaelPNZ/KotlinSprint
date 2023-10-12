845924c5141b4a9c3982b9a302d2e5ebdaf004f3
PozyalovMihail <pozyalov@gmail.com>
2023-10-12 12:35:46 +0300

    KS-13-2 Task_2 completed.

--- src/main/kotlin/lesson_13/lesson13-task2.kt
+++ src/main/kotlin/lesson_13/lesson13-task2.kt
@@ -1,2 +1,12 @@
 package lesson_13
 
+class PhoneBook2(val name: String, val phoneNumber: Long, val company: String?) {
+    fun print() {
+        println("-Имя: ${name}\n-Номер: ${phoneNumber}\n-Компания: ${company}")
+    }
+}
+
+fun main() {
+    val contact = PhoneBook2("Michael", 89998887799, null ?: "<не указано>")
+    contact.print()
+}
