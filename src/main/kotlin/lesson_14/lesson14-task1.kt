72a65b2d184e489f6152d022b52e9a9f871e090d
PozyalovMihail <pozyalov@gmail.com>
2023-10-18 16:48:43 +0300

    KS-14-1 Task_1 completed.

--- src/main/kotlin/lesson_14/lesson14-task1.kt
+++ src/main/kotlin/lesson_14/lesson14-task1.kt
@@ -1,2 +1,29 @@
 package lesson_14
 
+open class Liner (
+    val name: String,
+    val speed: Int,
+    val passengers: Int = 5000
+){}
+
+class Cargo (
+    name: String,
+    speed: Int,
+    passengers: Int,
+    val tonnage: Int = 40000
+) : Liner(name, speed, passengers) {}
+
+class Icebreaker (
+    name: String,
+    speed: Int,
+    passengers: Int,
+    val iceBreaking: String = "Может колоть лёд"
+) : Liner(name, speed, passengers) {}
+
+fun main() {
+    val liner = Liner("Лайнер", 15)
+    println("${liner.name}, скорость: ${liner.speed}, пассажиров: ${liner.passengers}")
+    val cargo = Cargo("Грузовой", 10, 1000)
+    println("${cargo.name}, скорость: ${cargo.speed}, пассажиров: ${cargo.passengers}, грузоподъемность: ${cargo.tonnage}")
+    val icebreaker = Icebreaker("Ледокол", 5, 700)
+    println("${icebreaker.name}, скорость: ${icebreaker.speed}, пассажиров: ${icebreaker.passengers}, ${icebreaker.iceBreaking}")}
