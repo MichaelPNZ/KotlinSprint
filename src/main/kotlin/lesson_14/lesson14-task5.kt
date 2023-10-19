62c0e35d2c9670927ebd8abdaa28863351e15720
PozyalovMihail <pozyalov@gmail.com>
2023-10-19 21:35:09 +0300

    KS-14-5 Task_5 .

--- src/main/kotlin/lesson_14/lesson14-task5.kt
+++ src/main/kotlin/lesson_14/lesson14-task5.kt
@@ -1,2 +1,42 @@
 package lesson_14
 
+open class Chat(
+//    val id: Int,
+    val name: String,
+    val text: String,
+    val listOfMessage: MutableMap<String, String> = mutableMapOf(),
+    val listOfTreadMessage: MutableMap<Int, String> = mutableMapOf(),
+) {
+    fun addMessage(name: String, text: String) {
+        listOfMessage.put(name, text)
+    }
+
+    fun addThreadMessage(name: String, text: String, parentMessageId: Int) {
+        listOfTreadMessage.put(parentMessageId, name)
+        listOfMessage.put(name, text)
+    }
+}
+
+class Message(
+//    id: Int,
+    text: String,
+    name: String,
+) : Chat(name, text) {
+
+}
+
+
+class ChildMessage(
+//    id: Int,
+    text: String,
+    name: String,
+) : Chat(name, text) {
+
+}
+
+fun main() {
+    val childMessage = ChildMessage("Katya", "adadsqawdq")
+    val message = Message("Petya", "assfadf")
+    val chat = Chat("Про осень", "d")
+    message.listOfMessage.forEach { println("${it.key}: ${it.value}") }
+}
