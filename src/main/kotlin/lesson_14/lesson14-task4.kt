3ccc2710ecdaf59efb0b6290a45959206adae06d
PozyalovMihail <pozyalov@gmail.com>
2023-10-19 11:40:58 +0300

    KS-14-4 Task_4 completed.

--- src/main/kotlin/lesson_14/lesson14-task4.kt
+++ src/main/kotlin/lesson_14/lesson14-task4.kt
@@ -1,2 +1,28 @@
 package lesson_14
 
+abstract class CelestialObject {
+    abstract val name: String
+    abstract val isAtmosphere: Boolean
+    abstract val isBoarding: Boolean
+}
+
+class Planet(
+    override val name: String,
+    override val isAtmosphere: Boolean,
+    override val isBoarding: Boolean,
+    val listOfSatellite: List<String>,
+) : CelestialObject() {}
+
+class Satellite(
+    override val name: String,
+    override val isAtmosphere: Boolean,
+    override val isBoarding: Boolean,
+) : CelestialObject() {}
+
+fun main() {
+    val satelliteOne = Satellite("Satellite One", true, false)
+    val satelliteTwo = Satellite("Satellite Two", false, true)
+
+    val planet = Planet("Honor", true, true, listOf(satelliteOne.name, satelliteTwo.name))
+    println("Планета ${planet.name} имеет спутники: ${planet.listOfSatellite} ")
+}
