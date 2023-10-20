a8437b9ee4c7bfb0175dde41c305eb9bd2d56239
PozyalovMihail <pozyalov@gmail.com>
2023-10-20 12:46:35 +0300

    KS-15-1 Task_1 completed.

--- src/main/kotlin/lesson_15/lesson15-task1.kt
+++ src/main/kotlin/lesson_15/lesson15-task1.kt
@@ -1,2 +1,42 @@
 package lesson_15
 
+interface Movable {
+
+    fun fly()
+    fun swim()
+
+}
+
+abstract class Animal : Movable { abstract val name: String }
+
+class Crucian (override val name: String ) : Animal() {
+
+    override fun fly() { println("$name не может летать") }
+    override fun swim() { println("$name может плавать") }
+
+}
+class Seagull (override val name: String ) : Animal() {
+
+    override fun fly() { println("$name может летать") }
+    override fun swim() { println("$name может плавать") }
+
+}
+
+class Duck (override val name: String ) : Animal() {
+
+    override fun fly() { println("$name может летать") }
+    override fun swim() { println("$name может плавать") }
+
+}
+
+fun main() {
+    val crucian = Crucian("Карась")
+    val seagull = Seagull("Чайка")
+    val duck = Duck("Утка")
+
+    crucian.swim()
+    seagull.swim()
+    seagull.fly()
+    duck.swim()
+    duck.fly()
+}
