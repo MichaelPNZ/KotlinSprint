8c0c43328f3fb081e1a99390cd7b3f835d28c4d1
PozyalovMihail <pozyalov@gmail.com>
2023-10-20 14:00:48 +0300

    KS-15-2 Task_2 completed.

--- src/main/kotlin/lesson_15/lesson15-task2.kt
+++ src/main/kotlin/lesson_15/lesson15-task2.kt
@@ -1,2 +1,39 @@
 package lesson_15
 
+
+abstract class WeatherStationStats{
+
+    abstract val temperature: Double
+    abstract val precipitationAmount: Int
+
+}
+
+class Temperature(
+    override val temperature: Double,
+    override val precipitationAmount: Int,
+) : WeatherStationStats()
+
+class PrecipitationAmount(
+    override val temperature: Double,
+    override val precipitationAmount: Int,
+) : WeatherStationStats()
+
+class WeatherServer(
+    override val temperature: Double,
+    override val precipitationAmount: Int,
+) : WeatherStationStats() {
+
+    fun sendMessage(i: Any) {
+        if (i == temperature) println("Температура: $i")
+        else println("Количество осадков: $precipitationAmount")
+    }
+}
+
+fun main() {
+    val temperature = Temperature(22.4, 200)
+    val precipitationAmount = PrecipitationAmount(11.1, 233)
+    val message = WeatherServer(temperature.temperature, precipitationAmount.precipitationAmount)
+
+    message.sendMessage(message.temperature)
+}
+
