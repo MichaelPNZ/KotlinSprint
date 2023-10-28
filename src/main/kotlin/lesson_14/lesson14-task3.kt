f9885ad1c9d31d55080fcd0ef59079e14d0883c2
PozyalovMihail <pozyalov@gmail.com>
2023-10-28 15:27:44 +0300

    KS-14-3 Task_3 fix.

--- src/main/kotlin/lesson_14/lesson14-task3.kt
+++ src/main/kotlin/lesson_14/lesson14-task3.kt
@@ -14,9 +14,9 @@ class Circle(
     override val color: String,
     val radius: Double
 ) : Figure() {
-    override fun calculatingSquare(): Double { return PI * radius.pow(2.0) }
+    override fun calculatingSquare(): Double = PI * radius.pow(2.0)
 
-    override fun calculatingPerimeter(): Double { return 2 * PI * radius }
+    override fun calculatingPerimeter(): Double = 2 * PI * radius
 }
 
 class Rectangle(
@@ -24,26 +24,33 @@ class Rectangle(
     val width: Double,
     val height: Double,
 ) : Figure() {
-    override fun calculatingSquare(): Double { return width * height }
+    override fun calculatingSquare(): Double = width * height
 
-    override fun calculatingPerimeter(): Double { return (width + height) * 2 }
+    override fun calculatingPerimeter(): Double = (width + height) * 2
 }
 
 fun main() {
-    val circleOne = Circle("White", 5.0)
-    val circleTwo = Circle("White", 2.7)
-    val circleThree = Circle("Black", 4.1)
-    val circleFour = Circle("Black", 9.3)
-    val rectangleOne = Rectangle("White", 7.6, 5.0)
-    val rectangleTwo = Rectangle("White", 4.1, 19.0)
-    val rectangleThree = Rectangle("Black", 2.9, 3.1)
-    val rectangleFour = Rectangle("Black", 6.3, 2.8)
+    val circleOne = Circle(WHITE_COLOR, 5.0)
+    val circleTwo = Circle(WHITE_COLOR, 2.7)
+    val circleThree = Circle(BLACK_COLOR, 4.1)
+    val circleFour = Circle(BLACK_COLOR, 9.3)
 
-    val allSquare =
-        circleOne.calculatingSquare() + circleTwo.calculatingSquare() + rectangleOne.calculatingSquare() + rectangleTwo.calculatingSquare()
-    val allPerimeter =
-        circleThree.calculatingPerimeter() + circleFour.calculatingPerimeter() + rectangleThree.calculatingPerimeter() + rectangleFour.calculatingPerimeter()
+    val rectangleOne = Rectangle(WHITE_COLOR, 7.6, 5.0)
+    val rectangleTwo = Rectangle(WHITE_COLOR, 4.1, 19.0)
+    val rectangleThree = Rectangle(BLACK_COLOR, 2.9, 3.1)
+    val rectangleFour = Rectangle(BLACK_COLOR, 6.3, 2.8)
 
-    println("Сумма периметров всех черных фигур: ${allPerimeter.roundToInt()}")
-    println("Сумма площадей всех белых фигур: ${allSquare.roundToInt()}")
+    val allFigure = listOf(circleOne, circleTwo, circleThree, circleFour, rectangleOne, rectangleTwo, rectangleThree, rectangleFour)
+
+    var allSquareWhiteFigure = 0.0
+    var allPerimeterBlackFigure = 0.0
+
+    for (i in allFigure.filter { it.color == WHITE_COLOR }) allSquareWhiteFigure += i.calculatingSquare()
+    for (i in allFigure.filter { it.color == BLACK_COLOR }) allPerimeterBlackFigure += i.calculatingPerimeter()
+
+    println("Сумма периметров всех черных фигур: ${allPerimeterBlackFigure.roundToInt()}")
+    println("Сумма площадей всех белых фигур: ${ allSquareWhiteFigure.roundToInt() }")
 }
+
+const val WHITE_COLOR = "White"
+const val BLACK_COLOR = "Black"
