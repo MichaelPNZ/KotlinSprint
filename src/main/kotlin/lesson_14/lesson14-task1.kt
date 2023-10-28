6bc4f7866a27a55271fb92e29a7558e6c09c97ed
PozyalovMihail <pozyalov@gmail.com>
2023-10-28 13:53:39 +0300

    KS-14-1 Task_1 fix.

--- src/main/kotlin/lesson_14/lesson14-task1.kt
+++ src/main/kotlin/lesson_14/lesson14-task1.kt
@@ -4,26 +4,26 @@ open class Liner (
     val name: String,
     val speed: Int,
     val passengers: Int = 5000
-){}
+)
 
 class Cargo (
     name: String,
     speed: Int,
     passengers: Int,
     val tonnage: Int = 40000
-) : Liner(name, speed, passengers) {}
+) : Liner(name, speed, passengers)
 
 class Icebreaker (
     name: String,
     speed: Int,
     passengers: Int,
-    val iceBreaking: String = "Может колоть лёд"
-) : Liner(name, speed, passengers) {}
+    val iceBreaking: Boolean,
+) : Liner(name, speed, passengers)
 
 fun main() {
     val liner = Liner("Лайнер", 15)
     println("${liner.name}, скорость: ${liner.speed}, пассажиров: ${liner.passengers}")
     val cargo = Cargo("Грузовой", 10, 1000)
     println("${cargo.name}, скорость: ${cargo.speed}, пассажиров: ${cargo.passengers}, грузоподъемность: ${cargo.tonnage}")
-    val icebreaker = Icebreaker("Ледокол", 5, 700)
-    println("${icebreaker.name}, скорость: ${icebreaker.speed}, пассажиров: ${icebreaker.passengers}, ${icebreaker.iceBreaking}")}
+    val icebreaker = Icebreaker("Ледокол", 5, 700, true)
+    println("${icebreaker.name}, скорость: ${icebreaker.speed}, пассажиров: ${icebreaker.passengers}, может колоть лёд: ${icebreaker.iceBreaking}")}
