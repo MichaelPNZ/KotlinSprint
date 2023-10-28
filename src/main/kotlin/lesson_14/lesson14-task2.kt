8cd6a5a61efa895d5115c237e0fd4124dce67bc5
PozyalovMihail <pozyalov@gmail.com>
2023-10-28 13:58:42 +0300

    KS-14-2 Task_2 fix.

--- src/main/kotlin/lesson_14/lesson14-task2.kt
+++ src/main/kotlin/lesson_14/lesson14-task2.kt
@@ -5,7 +5,7 @@ open class LinerOne(
     val speed: Int,
     val passengers: Int = 5000,
 ) {
-    open fun loadingMethod() { println("$name выдвигает горизонтальный трап со шкафута") }
+    open fun loadMethod() { println("$name выдвигает горизонтальный трап со шкафута") }
 }
 
 class CargoOne(
@@ -14,26 +14,31 @@ class CargoOne(
     passengers: Int,
     val tonnage: Int = 40000,
 ) : LinerOne(name, speed, passengers) {
-    override fun loadingMethod() { println("$name корабль активирует погрузочный кран") }
+    override fun loadMethod() { println("$name корабль активирует погрузочный кран") }
 }
 
 class IcebreakerOne(
     name: String,
     speed: Int,
     passengers: Int,
-    val iceBreaking: String = "Может колоть лёд",
+    val iceBreaking: Boolean,
 ) : LinerOne(name, speed, passengers) {
-    override fun loadingMethod() { println("$name открывает ворота со стороны кормы.") }
+    override fun loadMethod() { println("$name открывает ворота со стороны кормы.") }
 }
 
 fun main() {
     val linerOne = LinerOne("Лайнер", 15)
     println("${linerOne.name}, скорость: ${linerOne.speed}, пассажиров: ${linerOne.passengers}")
-    linerOne.loadingMethod()
+
+    linerOne.loadMethod()
+
     val cargoOne = CargoOne("Грузовой", 10, 1000)
     println("${cargoOne.name}, скорость: ${cargoOne.speed}, пассажиров: ${cargoOne.passengers}, грузоподъемность: ${cargoOne.tonnage}")
-    cargoOne.loadingMethod()
-    val icebreakerOne = IcebreakerOne("Ледокол", 5, 700)
-    println("${icebreakerOne.name}, скорость: ${icebreakerOne.speed}, пассажиров: ${icebreakerOne.passengers}, ${icebreakerOne.iceBreaking}")
-    icebreakerOne.loadingMethod()
+
+    cargoOne.loadMethod()
+
+    val icebreakerOne = IcebreakerOne("Ледокол", 5, 700, true)
+    println("${icebreakerOne.name}, скорость: ${icebreakerOne.speed}, пассажиров: ${icebreakerOne.passengers}, может колоть лёд: ${icebreakerOne.iceBreaking}")
+
+    icebreakerOne.loadMethod()
 }
