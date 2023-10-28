9ef2017dd4e254fc20d5d83720b251098cfc181e
PozyalovMihail <pozyalov@gmail.com>
2023-10-28 12:37:53 +0300

    KS-12-5 Task_5 fix.

--- src/main/kotlin/lesson_12/lesson12-task5.kt
+++ src/main/kotlin/lesson_12/lesson12-task5.kt
@@ -1,17 +1,14 @@
 package lesson_12
 
-class Weather3(val temperatureOfDay: Int, val temperatureOfNight: Int, val isRain: Boolean, val pressure: Int)
+class Weather4(val temperatureOfDay: Int, val temperatureOfNight: Int, val isRain: Boolean, val pressure: Int)
 
 fun main() {
     val random = kotlin.random.Random
-    val list = mutableListOf<Weather3>()
-    var medianTempOfDay = listOf<Int>()
-    var medianTempOfNight = listOf<Int>()
+    val list = mutableListOf<Weather4>()
     var medianRain = 0
-    var medianPressure = listOf<Int>()
 
     for (i in 1..10) {
-        list += Weather3(
+        list += Weather4(
             temperatureOfDay = (10..29).random(),
             temperatureOfNight = (5..20).random(),
             isRain = random.nextBoolean(),
@@ -20,19 +17,13 @@ fun main() {
     }
 
     for (i in list) {
-        medianTempOfDay += i.temperatureOfDay
-        medianTempOfNight += i.temperatureOfNight
-        medianPressure += i.pressure
-
-        if (i.isRain) {
-            medianRain += 1
-        }
+        if (i.isRain) { medianRain += 1 }
     }
 
     println(
-        """Среднее значение температур днем:${medianTempOfDay.average()}
-        |Среднее значение температур ночью: ${medianTempOfNight.average()}
-        |Среднее атмосферное давление: ${medianPressure.average()}
+        """Среднее значение температур днем:${list.map { it.temperatureOfDay }.average()}
+        |Среднее значение температур ночью: ${list.map { it.temperatureOfNight }.average()}
+        |Среднее атмосферное давление: ${list.map { it.pressure }.average()}
         |Дождливых дней: ${medianRain}
     """.trimMargin()
     )
