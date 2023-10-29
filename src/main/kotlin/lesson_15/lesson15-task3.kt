7fc7834d900fed052a281373b18aa755d3fede4b
PozyalovMihail <pozyalov@gmail.com>
2023-10-29 16:08:16 +0300

    KS-15-3 Task_3 completed.

--- src/main/kotlin/lesson_15/lesson15-task3.kt
+++ src/main/kotlin/lesson_15/lesson15-task3.kt
@@ -1,2 +1,46 @@
 package lesson_15
 
+abstract class Forum {
+    abstract val name: String
+    abstract val message: String
+
+    fun sendMessage() { println("${name}: ${message}") }
+
+    fun readMessage() {
+        println("Полозователь ${name} прочел сообщение.")
+    }
+}
+
+class Member(
+    override val name: String,
+    override val message: String,
+) : Forum()
+
+class Administrator(
+    override val name: String,
+    override val message: String,
+) : Forum() {
+
+    fun deletedMessage() { println("Сообщение <${message}> удалено") }
+
+    fun deletedMember() { println("Пользователь <${name}> удален") }
+
+}
+
+fun main() {
+    val memberOne = Member("Petr", "Hello!")
+    val memberTwo = Member("Yana", "Hello!! =)")
+    val memberThree = Member("Olya", "Hello!!!!!")
+
+    val administratorOne = Administrator("Ivan", "Hi!")
+    val administratorTwo = Administrator("Dima", "Hi!!!")
+    val administratorThree = Administrator("Katya", "Hi!!!!!!")
+
+    memberOne.sendMessage()
+    memberTwo.readMessage()
+    memberThree.readMessage()
+
+    administratorOne.sendMessage()
+    administratorTwo.deletedMember()
+    administratorThree.deletedMessage()
+}
