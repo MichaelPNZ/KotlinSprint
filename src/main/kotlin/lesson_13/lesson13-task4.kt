0e7eaea21c65a6c67ad60c5785fd7f26aaf824fe
PozyalovMihail <pozyalov@gmail.com>
2023-10-29 14:56:30 +0300

    KS-13-4 Task_4 fix.

--- src/main/kotlin/lesson_13/lesson13-task4.kt
+++ src/main/kotlin/lesson_13/lesson13-task4.kt
@@ -1,7 +1,7 @@
 package lesson_13
 
 class PhoneBook4(val name: String, val phoneNumber: Long?, val company: String? = null) {
-    fun print() { println("-Имя: ${ name }\n-Номер: ${ phoneNumber }\n-Компания: ${ if (company?.isEmpty() == true) "<не указано>" else company }") }
+    fun print() { println("-Имя: ${name}\n-Номер: ${phoneNumber}\n-Компания: ${company?.isEmpty()}") }
 }
 
 fun main() {
@@ -11,12 +11,17 @@ fun main() {
     while (isNext) {
         println("Введите имя")
         val name = readln()
+
         println("Введите номер")
         val phoneNumber = readln().toLongOrNull()
-        println("Введите имя компании, если ходите пропустить, нажмите Enter")
-        val company = readln()
 
-        if (phoneNumber != null) { listContact += PhoneBook4(name, phoneNumber, company) }
+        if (phoneNumber == null) println("Запись без номера телефона не будет добавлена")
+        else {
+            println("Введите имя компании, если ходите пропустить, нажмите Enter")
+            val company = readln()
+
+            listContact += PhoneBook4(name, phoneNumber, company)
+        }
 
         println("Продолжаем? Дальше/Стоп")
         val userAnswer = readln()
