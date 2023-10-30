ebca3cc0481ec71ac22560a62b969abe0e3c9acf
PozyalovMihail <pozyalov@gmail.com>
2023-10-30 12:43:28 +0300

    KS-16-1 Task_1 completed.

--- src/main/kotlin/lesson_16/lesson16-task1.kt
+++ src/main/kotlin/lesson_16/lesson16-task1.kt
@@ -1,2 +1,16 @@
 package lesson_16
 
+class Cube() {
+
+    private val randomNumber = (1..6).random()
+
+    fun printNumber() { println(randomNumber) }
+
+}
+
+fun main() {
+
+    val number = Cube()
+    number.printNumber()
+
+}
