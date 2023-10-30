c57ac4f7f020aee3bc0d496e07d1a602ee577c27
PozyalovMihail <pozyalov@gmail.com>
2023-10-30 14:58:43 +0300

    KS-17-1 Task_1 completed.

--- src/main/kotlin/lesson_17/lesson17-task1.kt
+++ src/main/kotlin/lesson_17/lesson17-task1.kt
@@ -1,2 +1,14 @@
 package lesson_17
 
+class Quiz() {
+
+    var question: String = ""
+        get() = field
+        set(value: String) {
+            field = value
+        }
+    var answer: String = ""
+        set(value: String) {
+            field = value
+        }
+}
