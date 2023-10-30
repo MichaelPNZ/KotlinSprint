03362a5cf502f548198c5332c492237cb3c3275b
PozyalovMihail <pozyalov@gmail.com>
2023-10-30 13:39:27 +0300

    KS-16-4 Task_4 completed.

--- src/main/kotlin/lesson_16/lesson16-task4.kt
+++ src/main/kotlin/lesson_16/lesson16-task4.kt
@@ -1,2 +1,21 @@
 package lesson_16
 
+class Order() {
+
+    private val orderNumber = 1
+    var orderStatus = "Заказ №${orderNumber} принят"
+
+    private fun changeOrderStatus(newOrderStatus: String) { orderStatus = newOrderStatus }
+
+    fun sendApplication(newOrderStatus: String){ changeOrderStatus(newOrderStatus) }
+
+}
+
+fun main() {
+
+    val order = Order()
+    println(order.orderStatus)
+    order.sendApplication("Заказ готов")
+    println(order.orderStatus)
+
+}
