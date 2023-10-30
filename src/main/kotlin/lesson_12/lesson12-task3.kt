815929b95a213df646dd39045c505a89c4996968
PozyalovMihail <pozyalov@gmail.com>
2023-10-30 12:45:19 +0300

    KS-12-3 Task_3 fix name Class.

--- src/main/kotlin/lesson_12/lesson12-task3.kt
+++ src/main/kotlin/lesson_12/lesson12-task3.kt
@@ -1,15 +1,15 @@
 package lesson_12
 
-class Weather3(val temperatureOfDay: Int, val temperatureOfNight: Int, val isRain: Boolean = false, val pressure: Int) {
+class Weather33(val temperatureOfDay: Int, val temperatureOfNight: Int, val isRain: Boolean = false, val pressure: Int) {
     fun print() {
         println("температура днем: ${temperatureOfDay}, температура ночью: ${temperatureOfNight}, давление: ${pressure}, дождь: ${isRain}.")
     }
 }
 
 fun main() {
-    val weather = Weather3(temperatureOfDay = 28, temperatureOfNight = 15, isRain = true, pressure = 770)
+    val weather = Weather33(temperatureOfDay = 28, temperatureOfNight = 15, isRain = true, pressure = 770)
     weather.print()
 
-    val weather2 = Weather3(temperatureOfDay = 5, temperatureOfNight = -10, pressure = 750)
+    val weather2 = Weather33(temperatureOfDay = 5, temperatureOfNight = -10, pressure = 750)
     weather2.print()
 }
