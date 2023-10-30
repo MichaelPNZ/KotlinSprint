39f173e16a037b85c78d01d53d732eda93b8119d
PozyalovMihail <pozyalov@gmail.com>
2023-10-30 13:17:07 +0300

    KS-16-3 Task_3 completed.

--- src/main/kotlin/lesson_16/lesson16-task3.kt
+++ src/main/kotlin/lesson_16/lesson16-task3.kt
@@ -1,2 +1,23 @@
 package lesson_16
 
+
+class User() {
+
+    private val login = "qwerty"
+    private val password = "zxcvb"
+
+    private fun getPassword() = password
+    fun getRelevancePassword(password: String) : Boolean {
+        println("Login: ${login}")
+        return password == getPassword()
+    }
+
+}
+
+fun main() {
+
+    val password = User()
+
+    println("Relevance password: ${password.getRelevancePassword("zxccb")}")
+
+}
