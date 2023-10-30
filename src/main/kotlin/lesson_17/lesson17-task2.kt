2ecdf4155a53e485d1c9b1dd8c5889f889c01744
PozyalovMihail <pozyalov@gmail.com>
2023-10-30 15:10:07 +0300

    KS-17-2 Task_2 completed.

--- src/main/kotlin/lesson_17/lesson17-task2.kt
+++ src/main/kotlin/lesson_17/lesson17-task2.kt
@@ -1,2 +1,21 @@
 package lesson_17
 
+class Ship() {
+
+    var name = "Vol'niy"
+        set(value) {
+            println("Изменять имя нельзя!")
+        }
+    val averageSpeed = 20
+    val homePort = "Vladivostok"
+
+}
+
+fun main() {
+
+    val ship = Ship()
+    println(ship.name)
+    ship.name = "sdad"
+    println(ship.name)
+
+}
