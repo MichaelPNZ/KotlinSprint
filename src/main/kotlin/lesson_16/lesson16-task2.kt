e4d34682df172cd3efb256c8242272eef3c525ea
PozyalovMihail <pozyalov@gmail.com>
2023-10-30 13:04:51 +0300

    KS-16-2 Task_2 completed.

--- src/main/kotlin/lesson_16/lesson16-task2.kt
+++ src/main/kotlin/lesson_16/lesson16-task2.kt
@@ -1,2 +1,23 @@
 package lesson_16
 
+import kotlin.math.pow
+
+class Circle(private val radius: Double) {
+    private val pi = 3.14
+
+    fun getSquare() {
+        println("Площадь круга равна: ${pi * radius.pow(2.0)}")
+    }
+
+    fun getCircumference() {
+        println("Длина окружности круга равна: ${2 * pi * radius}")
+    }
+}
+
+fun main() {
+
+    val circle = Circle(12.2)
+    circle.getSquare()
+    circle.getCircumference()
+
+}
