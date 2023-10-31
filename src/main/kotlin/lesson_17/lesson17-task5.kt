ec59cc19a2d2e854ed05f2ebaf7a7b5162f1d739
PozyalovMihail <pozyalov@gmail.com>
2023-10-31 20:20:23 +0300

    KS-17-5 Task_5 completed.

--- src/main/kotlin/lesson_17/lesson17-task5.kt
+++ src/main/kotlin/lesson_17/lesson17-task5.kt
@@ -1,2 +1,25 @@
 package lesson_17
 
+class User(private var log: String, private val pass: String) {
+
+    var login: String
+        get() = log
+        set(value) {
+            println("Вы успешно поменяли логин.")
+        }
+
+    var password: String
+        get() = pass.forEach { char -> print("*") }.toString()
+        set(value) {
+            println("Вы не можете изменить пароль")
+        }
+
+}
+
+fun main() {
+
+    val user = User("qwerty", "zxcvb")
+    user.login = "asas"
+    user.password
+
+}
