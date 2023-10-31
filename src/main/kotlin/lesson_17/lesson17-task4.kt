3099bd133eef9e4de59ff1c90651bce75fd31d70
PozyalovMihail <pozyalov@gmail.com>
2023-10-31 15:35:02 +0300

    KS-17-4 Task_4 completed.

--- src/main/kotlin/lesson_17/lesson17-task4.kt
+++ src/main/kotlin/lesson_17/lesson17-task4.kt
@@ -1,2 +1,25 @@
 package lesson_17
 
+class PackagePoint(private val numberPackage: Int) {
+
+    private var movementCount = 0
+
+    var currentLocation = ""
+        set(value) {
+            field = value
+            movementCount += 1
+            println("Посылка №${numberPackage}, текущая локация ${currentLocation}.")
+            println("Перемещение №${movementCount}.")
+        }
+
+}
+
+fun main() {
+
+    val packageOne = PackagePoint(122)
+    packageOne.currentLocation = "asasd"
+    packageOne.currentLocation = "asasd"
+    packageOne.currentLocation = "qefq"
+    packageOne.currentLocation = "dbdb"
+
+}
