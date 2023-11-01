ff66291007d95053c3086504cfd415f66f4160ee
PozyalovMihail <pozyalov@gmail.com>
2023-11-01 15:06:58 +0300

    KS-18-3 Task_3 completed.

--- src/main/kotlin/lesson_18/lesson18-task3.kt
+++ src/main/kotlin/lesson_18/lesson18-task3.kt
@@ -1,2 +1,81 @@
 package lesson_18
 
+open class Animal {
+
+    open fun play() = ""
+    open fun eat() = ""
+    open fun sleep() = ""
+
+}
+
+class Fox(
+    private val name: String,
+    private val food: String = "ягоды",
+) : Animal() {
+    override fun play(): String {
+        return "$name - играет"
+    }
+
+    override fun eat(): String {
+        return "$name - ест $food"
+    }
+
+    override fun sleep(): String {
+        return "$name - спит"
+    }
+}
+
+class Dog(
+    private val name: String,
+    private val food: String = "кости",
+) : Animal() {
+    override fun play(): String {
+        return "$name - играет"
+    }
+
+    override fun eat(): String {
+        return "$name - ест $food"
+    }
+
+    override fun sleep(): String {
+        return "$name - спит"
+    }
+}
+
+class Cat(
+    private val name: String,
+    private val food: String = "рыбу",
+) : Animal() {
+    override fun play(): String {
+        return "$name - играет"
+    }
+
+    override fun eat(): String {
+        return "$name - ест $food"
+    }
+
+    override fun sleep(): String {
+        return "$name - спит"
+    }
+}
+
+fun main() {
+
+    val animalOne = Fox("Лиса")
+    val animalTwo = Dog("Собака")
+    val animalThree = Cat("Кот")
+
+    val list = arrayOf<Animal>(animalOne, animalTwo, animalThree)
+
+    showAllAnimals(list)
+
+}
+
+fun showAllAnimals(notes: Array<Animal>) {
+    notes.forEach {
+        println(it.eat())
+        println(it.play())
+        println(it.sleep())
+    }
+}
+
