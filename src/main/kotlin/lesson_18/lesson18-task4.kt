43cd09c4d9100dfc517754316efb323b387fb435
PozyalovMihail <pozyalov@gmail.com>
2023-11-01 16:59:33 +0300

    KS-18-4 Task_4 completed.

--- src/main/kotlin/lesson_18/lesson18-task4.kt
+++ src/main/kotlin/lesson_18/lesson18-task4.kt
@@ -1,2 +1,37 @@
 package lesson_18
 
+import kotlin.math.pow
+
+open class Package {
+
+    open fun getTypePackage(): String = ""
+
+}
+
+class RectangularPackage(
+    private val height: Double,
+    private val width: Double,
+    private val length: Double,
+) : Package() {
+    override fun getTypePackage(): String {
+        return "Площадь поверхности посылки: ${2 * ((height * width) + (height * length) + (width * length))}"
+    }
+}
+
+class CubicPackage(
+    private val edgeLengths: Double,
+) : Package() {
+    override fun getTypePackage(): String {
+        return "Площадь поверхности посылки: ${6 * edgeLengths.pow(2.0)}"
+    }
+}
+
+fun main() {
+
+    val rectangularPackage = RectangularPackage(12.0, 3.9, 5.3)
+    val cubicPackage = CubicPackage(23.5)
+
+    println(rectangularPackage.getTypePackage())
+    println(cubicPackage.getTypePackage())
+
+}
