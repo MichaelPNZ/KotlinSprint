55e5ef9edc64c99c0ee690a2dbd7b9a91b6eb2ed
PozyalovMihail <pozyalov@gmail.com>
2023-11-01 14:46:00 +0300

    KS-18-2 Task_2 completed.

--- src/main/kotlin/lesson_18/lesson18-task2.kt
+++ src/main/kotlin/lesson_18/lesson18-task2.kt
@@ -1,2 +1,49 @@
 package lesson_18
 
+open class Dice {
+
+    open fun rollDice() = ""
+
+}
+
+class FourFaces(
+    private val countFaces: Int = (1..4).random(),
+) : Dice() {
+    override fun rollDice(): String {
+        return "Выпало число: $countFaces"
+    }
+}
+
+class SixFaces(
+    private val countFaces: Int = (1..6).random(),
+) : Dice() {
+    override fun rollDice(): String {
+        return "Выпало число: $countFaces"
+    }
+}
+
+class EightFaces(
+    private val countFaces: Int = (1..8).random(),
+) : Dice() {
+    override fun rollDice(): String {
+        return "Выпало число: $countFaces"
+    }
+}
+
+fun main() {
+
+    val diceOne = FourFaces()
+    val diceTwo = SixFaces()
+    val diceThree = EightFaces()
+
+    val list = arrayOf<Dice>(diceOne, diceTwo, diceThree)
+
+    showAllNotes(list)
+
+}
+
+fun showAllNotes(notes: Array<Dice>) {
+    notes.forEach {
+        println(it.rollDice())
+    }
+}
