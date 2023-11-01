e4bdaf1aeb19e1999a459dc4a6c93da5239a53a6
PozyalovMihail <pozyalov@gmail.com>
2023-11-01 14:20:31 +0300

    KS-18-1 Task_1 completed.

--- src/main/kotlin/lesson_18/lesson18-task1.kt
+++ src/main/kotlin/lesson_18/lesson18-task1.kt
@@ -1,2 +1,42 @@
 package lesson_18
 
+open class CRM() {
+
+    open fun getOrder() = ""
+
+}
+
+class OrderOne(
+    private val numberOrder: Int,
+    private val product: String,
+) : CRM() {
+    override fun getOrder(): String {
+        return "Заказ №$numberOrder, товар: $product"
+    }
+}
+
+class OrderTwo(
+    private val numberOrder: Int,
+    private val product: List<String>,
+) : CRM() {
+    override fun getOrder(): String {
+        return "Заказ №$numberOrder. Заказаны следующие товары: $product"
+    }
+}
+
+fun main() {
+
+    val orderFirst = OrderOne(1, "Phone")
+    val orderSecond = OrderTwo(2, listOf("Case", "cable", "sim"))
+
+    val list = arrayOf<CRM>(orderFirst, orderSecond)
+
+    showAllOrders(list)
+
+}
+
+fun showAllOrders(orders: Array<CRM>) {
+    orders.forEach {
+        println(it.getOrder())
+    }
+}
