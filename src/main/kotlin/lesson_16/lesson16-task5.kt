787a599818e93c33df3687098dc4d7bdda4b5c10
PozyalovMihail <pozyalov@gmail.com>
2023-11-02 00:27:56 +0300

    KS-16-5 Task_5 fix.

--- src/main/kotlin/lesson_16/lesson16-task5.kt
+++ src/main/kotlin/lesson_16/lesson16-task5.kt
@@ -1,35 +1,38 @@
 package lesson_16
 
-class Gamer() {
+class Gamer(val name: String, var health: Int, var power: Int) {
 
-    val name = "Billy"
-    private var health = 300
-    var power = 80
+    private var isDead = false
 
-    private fun healing() {
-        health += 20
-        println("Здоровье опустилось ниже 200, лечимся на 20 единиц(${health}).")
+    fun healing(healthPoint: Int) {
+        health += healthPoint
+        println("Лечимся на ${health} единиц.")
     }
 
-    fun damage() {
-        health -= power
-        println("Нанесено ${power} урона.")
+    fun damage(damagePoint: Int) {
+        health -= damagePoint
+        println("Нанесено ${damagePoint} урона.")
+        deadGamer()
+    }
 
-        if ( health <= 0) {
+    private fun deadGamer() {
+        if (health <= 0) {
             power = 0
             health = 0
+            isDead = true
             println("Игрок умер")
-        } else if (health < 200) healing()
+        }
     }
 }
 
 fun main() {
 
-    val gamer = Gamer()
-    gamer.damage()
-    gamer.damage()
-    gamer.damage()
-    gamer.damage()
-    gamer.damage()
+    val gamer = Gamer("Billy", 200, 50)
+    gamer.damage(57)
+    gamer.healing(66)
+    gamer.damage(51)
+    gamer.damage(50)
+    gamer.damage(49)
+    gamer.damage(62)
 
 }
