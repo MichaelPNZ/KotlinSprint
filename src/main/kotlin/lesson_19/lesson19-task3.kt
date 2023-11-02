8618097c8f7e207096489b9dfa42cad381a84876
PozyalovMihail <pozyalov@gmail.com>
2023-11-02 15:50:23 +0300

    KS-19-3 Task_3 completed.

--- src/main/kotlin/lesson_19/lesson19-task3.kt
+++ src/main/kotlin/lesson_19/lesson19-task3.kt
@@ -1,2 +1,30 @@
 package lesson_19
 
+enum class StarShip {
+    TAKE_OFF {
+        override fun action() {
+            TODO("Not yet implemented")
+        }
+    },
+    LANDING {
+        override fun action() {
+           // TODO добавить логику
+        }
+    },
+    SHOOT {
+        override fun action() {
+            println("отстреливаемся от астероида")
+        }
+    };
+
+    abstract fun action()
+
+}
+
+fun main() {
+
+    println(StarShip.TAKE_OFF.action())
+    println(StarShip.LANDING.action())
+    println(StarShip.SHOOT.action())
+
+}
