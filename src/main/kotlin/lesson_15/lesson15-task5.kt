3477c4ce3fa72fad1c3604b7b83a0d5678bbe053
PozyalovMihail <pozyalov@gmail.com>
2023-11-03 23:24:59 +0300

    KS-15-5 Task_5 fix.

--- src/main/kotlin/lesson_15/lesson15-task5.kt
+++ src/main/kotlin/lesson_15/lesson15-task5.kt
@@ -14,28 +14,16 @@ interface PassengerTransportation {
 
 }
 
-interface TrafficPermit {
-
-    fun getPermit()
-
-}
-
-abstract class Car : TrafficPermit, PassengerTransportation {
+abstract class Car : PassengerTransportation {
+    abstract val maxPassengers: Int
     abstract val passengersCount: Int
     abstract var permit: Boolean
 
-    override fun getPermit() {
-        if (passengersCount <= MAX_PASSENGERS_OF_PASSENGER_CAR) {
-            println("Разрешение на выезд получено")
-            permit = true
-        } else {
-            println("Разрешение на выезд не получено")
-        }
-    }
-
     override fun downloadPassengers() {
-        if (permit) println("Сели ${passengersCount} пассажир")
-        else println("${passengersCount} пассажира не вмещает, максимум ${MAX_PASSENGERS_OF_PASSENGER_CAR}!")
+        if (passengersCount <= maxPassengers) {
+            permit = true
+            println("Сели ${passengersCount} пассажир")
+        } else println("${passengersCount} пассажира не вмещает, максимум ${maxPassengers}!")
     }
 
     override fun unloadPassengers() {
@@ -44,28 +32,18 @@ abstract class Car : TrafficPermit, PassengerTransportation {
 }
 
 class Truck(
+    override val maxPassengers: Int,
     override val passengersCount: Int,
     val capacity: Int,
+    val maxCapacity: Int,
     override var permit: Boolean = false,
 ) : Car(), CargoTransportation {
 
-    override fun getPermit() {
-        if (passengersCount <= MAX_PASSENGERS_OF_TRUCK && capacity <= MAX_CAPACITY_OF_TRUCK) {
-            println("Разрешение на выезд получено")
-            permit = true
-        } else {
-            println("Разрешение на выезд не получено")
-        }
-    }
-
-    override fun downloadPassengers() {
-        if (permit) println("Сели ${passengersCount} пассажир")
-        else println("${passengersCount} пассажира не вмещает, максимум ${MAX_PASSENGERS_OF_TRUCK}!")
-    }
-
     override fun downloadCargo() {
-        if (capacity > MAX_CAPACITY_OF_TRUCK) println("${capacity}т. не вмещает, максимум ${MAX_CAPACITY_OF_TRUCK}т. груза!")
-        else if (permit) println("Загрузили ${capacity}т. груза")
+        if (capacity > maxCapacity) {
+            permit = true
+            println("${capacity}т. не вмещает, максимум ${maxCapacity}т. груза!")
+        } else if (permit) println("Загрузили ${capacity}т. груза")
     }
 
     override fun unloadCargo() {
@@ -74,38 +52,31 @@ class Truck(
 }
 
 class PassengerCar(
+    override val maxPassengers: Int,
     override val passengersCount: Int,
     override var permit: Boolean = false,
 ) : Car()
 
 fun main() {
 
-    val truck = Truck(1, 2)
-    truck.getPermit()
+    val truck = Truck(1, 1, 2, 2)
     truck.downloadPassengers()
     truck.downloadCargo()
     truck.unloadPassengers()
     truck.unloadCargo()
 
-    val truckTwo = Truck(2, 1)
-    truckTwo.getPermit()
+    val truckTwo = Truck(1, 2, 1, 2)
     truckTwo.downloadPassengers()
     truckTwo.downloadCargo()
     truckTwo.unloadPassengers()
     truckTwo.unloadCargo()
 
-    val car = PassengerCar(3)
-    car.getPermit()
+    val car = PassengerCar(3, 3)
     car.downloadPassengers()
     car.unloadPassengers()
 
-    val carTwo = PassengerCar(2)
-    carTwo.getPermit()
+    val carTwo = PassengerCar(3, 2)
     carTwo.downloadPassengers()
     carTwo.unloadPassengers()
 
 }
-
-const val MAX_PASSENGERS_OF_TRUCK = 1
-const val MAX_PASSENGERS_OF_PASSENGER_CAR = 3
-const val MAX_CAPACITY_OF_TRUCK = 2
