2bbdbe13d81888b7037157e314e204ddf21ee324
PozyalovMihail <pozyalov@gmail.com>
2023-11-03 23:33:39 +0300

    KS-17-3 Task_3 fix.

--- src/main/kotlin/lesson_17/lesson17-task3.kt
+++ src/main/kotlin/lesson_17/lesson17-task3.kt
@@ -1,7 +1,7 @@
 package lesson_17
 
 
-class Folder(private val name: String, private val fileCount: Int, private val isSecret: Boolean) {
+class Folder(val name: String, private val fileCount: Int, private val isSecret: Boolean) {
     val folderName: String
         get() = if (isSecret) "скрытая папка" else name
 
@@ -10,7 +10,7 @@ class Folder(private val name: String, private val fileCount: Int, private val i
 }
 
 fun main() {
-    val hiddenFolder = Folder("Моя папка", 6, true)
+    val hiddenFolder = Folder("Моя папка", 6, false)
 
     println("Имя папки: ${hiddenFolder.folderName}")
     println("Количество файлов в папке: ${hiddenFolder.folderFileCount}")
