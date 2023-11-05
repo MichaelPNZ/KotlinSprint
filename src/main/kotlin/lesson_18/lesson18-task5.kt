dd83fbceddc18e724391c7e2d20e4ff141ab94ee
PozyalovMihail <pozyalov@gmail.com>
2023-11-06 00:05:00 +0300

    KS-18-5 Task_5 fix.

--- src/main/kotlin/lesson_18/lesson18-task5.kt
+++ src/main/kotlin/lesson_18/lesson18-task5.kt
@@ -1,40 +1,74 @@
 package lesson_18
 
-import java.awt.Graphics
-
-open class Shape(val x: Float, val y: Float) {
-    open fun draw(g: Graphics) {}
+abstract class Shape(val x: Float, val y: Float) {
+    abstract fun drawInt()
+    abstract fun drawFloat()
 }
 
 class Point(x: Float, y: Float) : Shape(x, y) {
-    override fun draw(g: Graphics) {
-        g.fillOval(x.toInt(), y.toInt(), 1, 1)
+
+    override fun drawInt() {
+        println("Рисуем точку с координатами x:${x.toInt()}, y:${y.toInt()}.")
     }
+
+    override fun drawFloat() {
+        println("Рисуем точку с координатами x:$x, y:$y.")
+    }
+
 }
 
-class Square(x: Float, y: Float, val size: Int) : Shape(x, y) {
-    override fun draw(g: Graphics) {
-        g.fillRect(x.toInt(), y.toInt(), size, size)
+class Square(x: Float, y: Float, val size: Float) : Shape(x, y) {
+
+    override fun drawInt() {
+        println("Рисуем квадрат с координатами x:${x.toInt()}, y:${y.toInt()}, длинной стороны: ${size.toInt()}.")
     }
+
+    override fun drawFloat() {
+        println("Рисуем квадрат с координатами x:${x}, y:${y}, длинной стороны: $size.")
+    }
+
 }
 
-class Circle(x: Float, y: Float, val radius: Int) : Shape(x, y) {
-    override fun draw(g: Graphics) {
-        val diameter = radius * 2
-        g.fillOval(x.toInt() - radius, y.toInt() - radius, diameter, diameter)
+class Circle(x: Float, y: Float, val radius: Float) : Shape(x, y) {
+
+    override fun drawInt() {
+        println("Рисуем круг с координатами x:${x.toInt()}, y:${y.toInt()} и радиусом: ${radius.toInt()}.")
     }
+
+    override fun drawFloat() {
+        println("Рисуем круг с координатами x:${x}, y:${y} и радиусом: $radius.")
+    }
+
 }
 
-class Screen(val width: Int, val height: Int) {
-    private val shapes = mutableListOf<Shape>()
+class Screen() {
 
-    fun draw(shape: Shape) {
-        shapes.add(shape)
+    fun drawInt(shape: Shape) {
+        shape.drawInt()
     }
 
-    fun render(g: Graphics) {
-        for (shape in shapes) {
-            shape.draw(g)
-        }
+    fun drawFloat(shape: Shape) {
+        shape.drawFloat()
     }
+
+}
+
+fun main() {
+
+    val screen = Screen()
+
+    val point = Point(2.0F, 2.0F)
+    val square = Square(5.2F, 13.4F, 2.3F)
+    val circle = Circle(35.2F, 22.1F, 23.6F)
+
+    screen.drawInt(point)
+    screen.drawInt(square)
+    screen.drawInt(circle)
+
+    println()
+
+    screen.drawFloat(point)
+    screen.drawFloat(square)
+    screen.drawFloat(circle)
+
 }
