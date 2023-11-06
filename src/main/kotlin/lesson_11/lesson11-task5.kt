ebea8443805017c02b33e4a53b2be76182b03633
PozyalovMihail <pozyalov@gmail.com>
2023-11-06 17:40:29 +0300

    KS-11-5 Task_5 completed.

--- src/main/kotlin/lesson_11/lesson11-task5.kt
+++ src/main/kotlin/lesson_11/lesson11-task5.kt
@@ -1,2 +1,55 @@
 package lesson_11
 
+class MemberForum(
+    val userId: Int,
+    val userName: String,
+)
+
+class MessageForum(
+    val authorId: Int,
+    val message: String,
+)
+
+class Forum {
+
+    private val userList = mutableListOf<MemberForum>()
+    private val messageList = mutableListOf<MessageForum>()
+
+    fun createNewUser(name: String) {
+        val id = userList.size + 1
+        val newUser = MemberForum(id, name)
+        userList.add(newUser)
+    }
+
+    fun createNewMessage(authorId: Int, message: String) {
+        val checkId = userList.any { it.userId == authorId }
+
+        if (checkId) {
+            val newMessage = MessageForum(authorId, message)
+            messageList.add(newMessage)
+        } else {
+            println("Пользователя с id $authorId не существует.")
+        }
+
+    }
+
+    fun printThread() {
+        for (message in messageList) {
+            val author = userList.find { it.userId == message.authorId }
+            println("${author?.userName ?: "Unknown User"}: ${message.message}")
+        }
+    }
+
+}
+
+fun main() {
+
+    val forum = Forum()
+
+    forum.createNewUser("Misha")
+    forum.createNewUser("Sergey")
+    forum.createNewMessage(0, "Hello!")
+    forum.createNewMessage(1, "Hello!!!")
+    forum.printThread()
+
+}
