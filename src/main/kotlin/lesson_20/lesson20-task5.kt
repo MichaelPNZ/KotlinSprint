7237b17ede6b8c220895fcfe6967a62b55503ea0
PozyalovMihail <pozyalov@gmail.com>
2023-11-07 17:26:44 +0300

    KS-20-5 Task_5 completed.

--- src/main/kotlin/lesson_20/lesson20-task5.kt
+++ src/main/kotlin/lesson_20/lesson20-task5.kt
@@ -1,2 +1,22 @@
 package lesson_20
 
+class Robot(val listString: List<String>) {
+
+    fun say(): String {
+        return listString.shuffled().first()
+    }
+
+    val invertString: () -> Unit = {
+        println(say().reversed())
+    }
+
+}
+
+fun main() {
+
+    val string = Robot(listOf("Hello!", "Bay", "Yes", "No"))
+
+    println(string.say())
+    string.invertString()
+
+}
