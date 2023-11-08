51958ed22d53973c1112c3755d9c066bd9e25af9
PozyalovMihail <pozyalov@gmail.com>
2023-11-08 14:57:56 +0300

    KS-21-2 Task_2 completed.

--- src/main/kotlin/lesson_21/lesson21-task2.kt
+++ src/main/kotlin/lesson_21/lesson21-task2.kt
@@ -1,2 +1,20 @@
 package lesson_21
 
+
+fun Collection<Int>.evenNumbersSum(list: List<Int>) {
+    var sum = 0
+    list.forEach { i ->
+        if (i % 2 == 0) {
+            sum += i
+        }
+    }
+    println(sum)
+}
+
+fun main() {
+
+    val listNumber = listOf<Int>()
+
+    listNumber.evenNumbersSum(listOf(1, 2, 3, 4, 5, 6, 7))
+
+}
