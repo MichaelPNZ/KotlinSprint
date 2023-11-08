7fa9d6714611d9080f113184a301f1cea637765c
PozyalovMihail <pozyalov@gmail.com>
2023-11-08 14:10:54 +0300

    KS-20-3 Task_3 fix.

--- src/main/kotlin/lesson_20/lesson20-task3.kt
+++ src/main/kotlin/lesson_20/lesson20-task3.kt
@@ -1,11 +1,15 @@
 package lesson_20
 
-class Gamer (val isKey: Boolean)
+class Gamer (val hasKey: Boolean) {
 
-val openDoor: (Gamer) -> Unit = {
-    if (it.isKey) println("Игрок открыл дверь")
-    else println("Дверь заперта, у вас нет ключа")
+    fun hasKey(): Boolean { return hasKey }
+
 }
+
+    val openDoor = { gamer: Gamer ->
+        if (gamer.hasKey()) println("Игрок открыл дверь")
+        else println("Дверь заперта, у вас нет ключа")
+    }
 
 fun main() {
 
