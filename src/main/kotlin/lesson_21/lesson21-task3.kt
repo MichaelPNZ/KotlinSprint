815906bac3d812759dbb7445c0ec7267f539772d
PozyalovMihail <pozyalov@gmail.com>
2023-11-08 15:12:34 +0300

    KS-21-3 Task_3 completed.

--- src/main/kotlin/lesson_21/lesson21-task3.kt
+++ src/main/kotlin/lesson_21/lesson21-task3.kt
@@ -1,2 +1,31 @@
 package lesson_21
 
+class Gamer(
+    var name: String,
+    var currentHealth: Int,
+    val maxHealth: Int,
+)
+
+val healing: (Gamer) -> Unit = { gamer ->
+    val healthToRestore = gamer.maxHealth - gamer.currentHealth
+    if (healthToRestore > 0) {
+        println("${gamer.name} использует лечебное зелье.")
+        gamer.currentHealth = gamer.maxHealth
+        println("${gamer.name} восстановил $healthToRestore здоровья. Теперь его здоровье: ${gamer.maxHealth}")
+    } else {
+        println("${gamer.name} уже полностью здоров.")
+    }
+}
+
+fun Gamer.isHealthy() : Boolean {
+    return currentHealth == maxHealth
+}
+
+fun main() {
+
+    val gamer = Gamer("Person", 300, 500)
+    healing(gamer)
+    healing(gamer)
+    println("здоровье игрока равно максимальному здоровью: ${gamer.isHealthy()}")
+
+}
