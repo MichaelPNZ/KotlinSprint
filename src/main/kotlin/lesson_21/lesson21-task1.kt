f6ccbb6c5707c2e9d904d53ecc2d2153ca025938
PozyalovMihail <pozyalov@gmail.com>
2023-11-08 14:40:05 +0300

    KS-21-1 Task_1 completed.

--- src/main/kotlin/lesson_21/lesson21-task1.kt
+++ src/main/kotlin/lesson_21/lesson21-task1.kt
@@ -1,2 +1,20 @@
 package lesson_21
 
+fun String.vowelCount(string: String): Int {
+    var vowels = 0
+
+    for (char in string) {
+        if (char.lowercase() in "aeiou") {
+            vowels++
+        }
+    }
+
+    return vowels
+}
+
+fun main() {
+
+    val string = String()
+    println(string.vowelCount("hello"))
+
+}
