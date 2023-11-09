c82466a91e7e2b8651c5048c9a319a162e4b208f
PozyalovMihail <pozyalov@gmail.com>
2023-11-09 15:19:37 +0300

    KS-22-4 Task_4 completed.

--- src/main/kotlin/lesson_22/lesson22-task4.kt
+++ src/main/kotlin/lesson_22/lesson22-task4.kt
@@ -1,2 +1,32 @@
 package lesson_22
 
+
+class ViewMadel(private val mainScreenState: MainScreenState) {
+
+    data class MainScreenState(
+        val data: String,
+        val isLoading: Boolean = false,
+    )
+
+    fun loadData() {
+
+        println("отсутствие данных")
+        if (mainScreenState.data.isNotEmpty()) {
+            println("загрузка данных")
+            val buffer = mainScreenState.copy(isLoading = true)
+            println(buffer)
+            println("наличие загруженных данных")
+        }
+
+    }
+
+}
+
+fun main() {
+
+    val data = ViewMadel.MainScreenState("asdad")
+    val viewState = ViewMadel(data)
+
+    viewState.loadData()
+
+}
