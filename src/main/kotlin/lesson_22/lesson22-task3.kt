93c9d4175147c4a78f4d127bb1256e22b49767cb
PozyalovMihail <pozyalov@gmail.com>
2023-11-09 14:31:39 +0300

    KS-22-3 Task_3 completed.

--- src/main/kotlin/lesson_22/lesson22-task3.kt
+++ src/main/kotlin/lesson_22/lesson22-task3.kt
@@ -1,2 +1,19 @@
 package lesson_22
 
+data class Book(
+    val bookName: String,
+    val bookAuthor: String,
+    val pageCount: Int,
+)
+
+fun main() {
+
+    val book1 = Book("Harry Potter", "J. K. Rowling", 500)
+
+    val bookName = book1.component1()
+    val bookAuthor = book1.component2()
+    val pageCount = book1.component3()
+
+    println("$bookName, $bookAuthor, $pageCount")
+
+}
