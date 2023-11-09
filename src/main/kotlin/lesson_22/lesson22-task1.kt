563efaefe86ae9d6c429b23b7329eff67d4cf14a
PozyalovMihail <pozyalov@gmail.com>
2023-11-09 13:58:02 +0300

    KS-22-1 Task_1 completed.

--- src/main/kotlin/lesson_22/lesson22-task1.kt
+++ src/main/kotlin/lesson_22/lesson22-task1.kt
@@ -1,2 +1,24 @@
 package lesson_22
 
+class RegularBook(
+    val bookName: String,
+    val bookAuthor: String,
+)
+
+data class DataBook(
+    val bookName: String,
+    val bookAuthor: String,
+)
+
+fun main() {
+
+    val book1 = RegularBook("Harry Potter", "J. K. Rowling")
+    val book2 = RegularBook("Harry Potter", "J. K. Rowling")
+
+    val book3 = DataBook("Harry Potter", "J. K. Rowling")
+    val book4 = DataBook("Harry Potter", "J. K. Rowling")
+
+    println(book1 == book2)
+    println(book3 == book4)
+
+}
