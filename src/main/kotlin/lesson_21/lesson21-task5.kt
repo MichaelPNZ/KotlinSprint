d03ad87814b33d0ecac6605817b7a854cae28d5b
PozyalovMihail <pozyalov@gmail.com>
2023-11-09 11:42:00 +0300

    KS-21-5 Task_5 completed.

--- src/main/kotlin/lesson_21/lesson21-task5.kt
+++ src/main/kotlin/lesson_21/lesson21-task5.kt
@@ -1,2 +1,24 @@
 package lesson_21
 
+interface Map {}
+
+fun Map.maxCategory(skills: kotlin.collections.Map<String, Int>): String {
+
+    val maxValueExp = skills.values.max()
+    var maxSkillName = ""
+
+    for (i in skills) {
+        if (i.value == maxValueExp) { maxSkillName = i.key }
+    }
+
+    return maxSkillName
+}
+
+class Person : Map
+
+fun main() {
+
+    val skill = Person()
+    println(skill.maxCategory(mapOf("aaaaa" to 10, "ppppp" to 20, "ssssss" to 20)))
+
+}
