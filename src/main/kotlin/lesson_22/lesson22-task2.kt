196e8692372f82d0516da1e0956c4667b6c4870d
PozyalovMihail <pozyalov@gmail.com>
2023-11-09 14:11:26 +0300

    KS-22-2 Task_2 completed.

--- src/main/kotlin/lesson_22/lesson22-task2.kt
+++ src/main/kotlin/lesson_22/lesson22-task2.kt
@@ -1,2 +1,22 @@
 package lesson_22
 
+class BookRegular(
+    val bookName: String,
+    val bookAuthor: String,
+)
+
+data class BookData(
+    val bookName: String,
+    val bookAuthor: String,
+)
+
+fun main() {
+
+    val book1 = BookRegular("Harry Potter", "J. K. Rowling")
+
+    val book2 = BookData("Harry Potter", "J. K. Rowling")
+
+    println(book1) // Будет распечатано строковое представление объекта по умолчанию.
+    println(book2) // Получаем переопределенную функцию toString()
+
+}
