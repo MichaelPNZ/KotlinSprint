54095dd506cb6a57454bc8a7f8821561e6c83e6f
PozyalovMihail <pozyalov@gmail.com>
2023-11-10 15:07:42 +0300

    KS-21-5 Task_5 fix.

--- src/main/kotlin/lesson_21/lesson21-task5.kt
+++ src/main/kotlin/lesson_21/lesson21-task5.kt
@@ -1,24 +1,27 @@
 package lesson_21
 
+
 interface Map {}
 
-fun Map.maxCategory(skills: kotlin.collections.Map<String, Int>): String {
-
-    val maxValueExp = skills.values.max()
+fun Map.maxCategory(): String {
+    val person = Person()
+    val maxValueExp = person.skill.values.max()
     var maxSkillName = ""
 
-    for (i in skills) {
+    for (i in person.skill) {
         if (i.value == maxValueExp) { maxSkillName = i.key }
     }
 
     return maxSkillName
 }
 
-class Person : Map
+class Person : Map {
+    val skill = mapOf("aaaaa" to 10, "ppppp" to 20, "ssssss" to 20)
+}
 
 fun main() {
 
     val skill = Person()
-    println(skill.maxCategory(mapOf("aaaaa" to 10, "ppppp" to 20, "ssssss" to 20)))
+    println(skill.maxCategory())
 
 }
